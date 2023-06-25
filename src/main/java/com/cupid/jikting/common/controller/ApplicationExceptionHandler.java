a235e32d61595ffeb04a68110fcdf97e9465177d
jeonyuneo <piq2255@gmail.com>
2023-06-25 22:28:36 +0900

    [SP-21] feat: ApplicationExceptionHandler 추가

---
+++ src/main/java/com/cupid/jikting/common/controller/ApplicationExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cupid.jikting.common.controller;
+
+import com.cupid.jikting.common.dto.ErrorResponse;
+import com.cupid.jikting.common.error.ApplicationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(ApplicationException.class)
+    protected ResponseEntity<ErrorResponse> handleApplicationException(ApplicationException exception) {
+        log.info("{}: {}", exception.getClass().getSimpleName(), exception.getMessage(), exception);
+        return ResponseEntity.status(exception.getStatus()).body(ErrorResponse.from(exception));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        log.info("{}: {}", exception.getClass().getSimpleName(), exception.getMessage(), exception);
+        return ResponseEntity.internalServerError().body(ErrorResponse.create());
+    }
+}
