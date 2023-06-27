59cff69ba729265b758efbfb87255ed9057c7349
smc2315 <choeyongtae085@gmail.com>
2023-06-27 16:51:22 +0900

    [SP-25] feat: MemberResponse 추가

---
+++ src/main/java/com/cupid/jikting/meeting/dto/MemberResponse.java
@@ -0,0 +1,20 @@
+package com.cupid.jikting.meeting.dto;
+
+import java.util.List;
+
+public class MemberResponse {
+
+	private String username;
+	private List<ImageResponse> images;
+	private int age;
+	private String mbti;
+	private String residence;
+	private String company;
+	private boolean isSmoke;
+	private String drinkStatus;
+	private int height;
+	private String description;
+	private List<PersonalityResponse> personalities;
+	private List<HobbyResponse> hobbies;
+	private String school;
+}
