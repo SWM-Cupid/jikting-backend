f0b80a167cfd0c422da0b7ca90ab8fa12215f189
jeonyuneo <piq2255@gmail.com>
2023-06-27 17:20:19 +0900

    [SP-23] feat: MemberService 회원가입 추가

---
+++ src/main/java/com/cupid/jikting/member/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.cupid.jikting.member.service;
+
+import com.cupid.jikting.member.dto.SignupRequest;
+
+public class MemberService {
+
+    public void signup(SignupRequest signupRequest) {
+    }
+}
