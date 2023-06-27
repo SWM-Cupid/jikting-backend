49886fd1dec9513aaf66df05f447e9d6c79f3d0a
jeonyuneo <piq2255@gmail.com>
2023-06-27 17:17:47 +0900

    [SP-23] test: MemberControllerTest 회원가입 성공 테스트 추가

---
+++ src/test/java/com/cupid/jikting/member/controller/MemberControllerTest.java
@@ -0,0 +1,50 @@
+package com.cupid.jikting.member.controller;
+
+import com.cupid.jikting.ApiDocument;
+import com.cupid.jikting.member.dto.SignupRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+public class MemberControllerTest extends ApiDocument {
+
+    public static final String CONTEXT_PATH = "/api/v1";
+    private static final String USERNAME = "아이디";
+    private static final String PASSWORD = "비밀번호";
+    private static final String NAME = "이름";
+    private static final String PHONE = "전화번호";
+
+    @Test
+    void 회원가입_성공() throws Exception {
+        // given
+        SignupRequest signupRequest = SignupRequest.builder()
+                .username(USERNAME)
+                .password(PASSWORD)
+                .name(NAME)
+                .phone(PHONE)
+                .build();
+        // when
+        ResultActions resultActions = 회원가입_요청(signupRequest);
+        // then
+        회원가입_요청_성공(resultActions);
+    }
+
+    private ResultActions 회원가입_요청(SignupRequest signupRequest) throws Exception {
+        return mockMvc.perform(post(CONTEXT_PATH + "/members")
+                .contextPath(CONTEXT_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(signupRequest)));
+    }
+
+    private void 회원가입_요청_성공(ResultActions resultActions) throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(toDocument("signup-success"));
+    }
+}
