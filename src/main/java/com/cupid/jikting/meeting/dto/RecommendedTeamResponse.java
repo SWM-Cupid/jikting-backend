887e0ea9cce296b8e51069b577d11a95f878dce8
smc2315 <choeyongtae085@gmail.com>
2023-06-27 16:51:46 +0900

    [SP-25] feat: RecommendedTeamResponse 추가

--- src/main/java/com/cupid/jikting/meeting/dto/RecommendedTeamResponse.java
+++ src/main/java/com/cupid/jikting/meeting/dto/RecommendedTeamResponse.java
@@ -1,4 +1,10 @@
 package com.cupid.jikting.meeting.dto;
 
+import java.util.List;
+
 public class RecommendedTeamResponse {
+
+	private Long teamId;
+	private List<MemberResponse> members;
+	private List<PersonalityResponse> personalities;
 }
