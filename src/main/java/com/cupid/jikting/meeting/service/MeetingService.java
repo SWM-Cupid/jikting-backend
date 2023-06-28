26e07cdda3e933e00e3f1f9bdce213bf666c0912
smc2315 <choeyongtae085@gmail.com>
2023-06-28 12:43:59 +0900

    [SP-25] feat: MeetingService 추가

---
+++ src/main/java/com/cupid/jikting/meeting/service/MeetingService.java
@@ -0,0 +1,13 @@
+package com.cupid.jikting.meeting.service;
+
+import org.springframework.stereotype.Service;
+
+import com.cupid.jikting.meeting.dto.RecommendedTeamResponse;
+
+@Service
+public class MeetingService {
+
+	public RecommendedTeamResponse getRecommendedTeam(Long teamId) {
+		return null;
+	}
+}
