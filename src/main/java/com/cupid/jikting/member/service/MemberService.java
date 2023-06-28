f1f2bc9b19a1a6bc33ed372bcbf1a6f49d4b383f
jeonyuneo <piq2255@gmail.com>
2023-06-28 21:39:50 +0900

    [SP-26] fix: MemberService 서비스 어노테이션 추가

--- src/main/java/com/cupid/jikting/member/service/MemberService.java
+++ src/main/java/com/cupid/jikting/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.cupid.jikting.member.service;
 
 import com.cupid.jikting.member.dto.SignupRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
 
     public void signup(SignupRequest signupRequest) {
