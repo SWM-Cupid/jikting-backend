2ebfd947e9a911bd65524a8eb5f3597516568c67
smc2315 <choeyongtae085@gmail.com>
2023-06-28 19:57:41 +0900

    [SP-25] refactor: username, residence 네이밍 수정 및 필요없는 dto 삭제 반영(String으로 변경)

--- src/main/java/com/cupid/jikting/recommend/dto/MemberResponse.java
+++ src/main/java/com/cupid/jikting/recommend/dto/MemberResponse.java
@@ -10,17 +10,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberResponse {
 
-	private String username;
-	private List<ImageResponse> images;
-	private int age;
-	private String mbti;
-	private String residence;
-	private String company;
-	private boolean isSmoke;
-	private String drinkStatus;
-	private int height;
-	private String description;
-	private List<PersonalityResponse> personalities;
-	private List<HobbyResponse> hobbies;
-	private String school;
+    private String nickname;
+    private List<ImageResponse> images;
+    private int age;
+    private String mbti;
+    private String address;
+    private String company;
+    private boolean isSmoke;
+    private String drinkStatus;
+    private int height;
+    private String description;
+    private List<String> personalities;
+    private List<String> hobbies;
+    private String college;
 }
