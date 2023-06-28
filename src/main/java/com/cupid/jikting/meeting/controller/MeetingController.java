2daeea6a8cc17f2bd3885176de116b9e42a59dbf
smc2315 <choeyongtae085@gmail.com>
2023-06-28 12:41:59 +0900

    [SP-25] feat: MeetingController 추천팀 조회 추가

--- src/main/java/com/cupid/jikting/meeting/controller/MeetingController.java
+++ src/main/java/com/cupid/jikting/meeting/controller/MeetingController.java
@@ -1,4 +1,26 @@
 package com.cupid.jikting.meeting.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cupid.jikting.meeting.dto.RecommendedTeamResponse;
+import com.cupid.jikting.meeting.service.MeetingService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/meetings")
 public class MeetingController {
+
+	private final MeetingService meetingService;
+
+	@GetMapping("/recommended-teams/{teamId}")
+	public ResponseEntity<RecommendedTeamResponse> getRecommendedTeam(@PathVariable Long teamId) {
+		return ResponseEntity.ok().body(meetingService.getRecommendedTeam(teamId));
+	}
+
 }
