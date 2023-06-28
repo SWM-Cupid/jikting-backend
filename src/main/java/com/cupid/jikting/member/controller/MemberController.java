5b080fd5c6bdc289c9c7d0599aa2090a06ad8154
jeonyuneo <piq2255@gmail.com>
2023-06-28 22:27:58 +0900

    [SP-26] feat: MemberController 회원수정 추가

--- src/main/java/com/cupid/jikting/member/controller/MemberController.java
+++ src/main/java/com/cupid/jikting/member/controller/MemberController.java
@@ -1,13 +1,11 @@
 package com.cupid.jikting.member.controller;
 
+import com.cupid.jikting.member.dto.MemberUpdateRequest;
 import com.cupid.jikting.member.dto.SignupRequest;
 import com.cupid.jikting.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +17,12 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<Void> signup(@RequestBody SignupRequest signupRequest) {
         memberService.signup(signupRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> update(@RequestBody MemberUpdateRequest memberUpdateRequest) {
+        memberService.update(memberUpdateRequest);
         return ResponseEntity.ok().build();
     }
 }
