6c717116d22ce17a6d394a2162e5ae461d406914
smc2315 <choeyongtae085@gmail.com>
2023-06-28 12:39:10 +0900

    [SP-25] feat: dto annotation 추가

--- src/main/java/com/cupid/jikting/meeting/dto/ImageResponse.java
+++ src/main/java/com/cupid/jikting/meeting/dto/ImageResponse.java
@@ -1,5 +1,15 @@
 package com.cupid.jikting.meeting.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageResponse {
 	private boolean isMain;
 	private Long memberId;
