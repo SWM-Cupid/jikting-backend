e2cd38c331e76a00c2c3904c06ad1fe5ae77663b
jeonyuneo <piq2255@gmail.com>
2023-06-29 16:49:03 +0900

    [SP-24] feat: ImageResponse 추가

---
+++ src/main/java/com/cupid/jikting/member/dto/ImageResponse.java
@@ -0,0 +1,13 @@
+package com.cupid.jikting.member.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ImageResponse {
+
+    private String url;
+    private String order;
+}
