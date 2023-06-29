8a97a1fa1b70f0a278c9694de3c62817918b0bca
smc2315 <choeyongtae085@gmail.com>
2023-06-29 13:48:39 +0900

    [SP-115] style: import 정리

--- src/main/java/com/cupid/jikting/recommend/controller/RecommendController.java
+++ src/main/java/com/cupid/jikting/recommend/controller/RecommendController.java
@@ -4,10 +4,7 @@ import com.cupid.jikting.recommend.dto.RecommendedTeamResponse;
 import com.cupid.jikting.recommend.service.RecommendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
