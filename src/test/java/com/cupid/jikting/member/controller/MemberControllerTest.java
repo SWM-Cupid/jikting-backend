ac67979d29841d660124d5a2c359f2e22e1d1aea
jeonyuneo <piq2255@gmail.com>
2023-07-01 17:17:09 +0900

    [SP-27] test: MemberControllerTest 회원 탈퇴 비밀번호불일치 실패 테스트 추가

--- src/test/java/com/cupid/jikting/member/controller/MemberControllerTest.java
+++ src/test/java/com/cupid/jikting/member/controller/MemberControllerTest.java
@@ -293,6 +293,16 @@ public class MemberControllerTest extends ApiDocument {
         회원_탈퇴_요청_회원정보찾기_실패(resultActions);
     }
 
+    @Test
+    void 회원_탈퇴_비밀번호불일치_실패() throws Exception {
+        // given
+        willThrow(passwordNotEqualException).given(memberService).withdraw(anyLong(), any(PasswordRequest.class));
+        // when
+        ResultActions resultActions = 회원_탈퇴_요청();
+        // then
+        회원_탈퇴_요청_비밀번호불일치_실패(resultActions);
+    }
+
     private ResultActions 회원_가입_요청() throws Exception {
         return mockMvc.perform(post(CONTEXT_PATH + DOMAIN_ROOT_PATH)
                 .contextPath(CONTEXT_PATH)
@@ -443,5 +453,12 @@ public class MemberControllerTest extends ApiDocument {
                         .andExpect(status().isBadRequest())
                         .andExpect(content().json(toJson(ErrorResponse.from(memberNotFoundException)))),
                 "delete-member-not-found-member-fail");
+    }
+
+    private void 회원_탈퇴_요청_비밀번호불일치_실패(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().json(toJson(ErrorResponse.from(passwordNotEqualException)))),
+                "delete-member-not-equal-password-fail");
     }
 }
