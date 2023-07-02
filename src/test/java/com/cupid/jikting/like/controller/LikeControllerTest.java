74873be47c3dd7dfec7a27a097133da353d05b20
smc2315 <choeyongtae085@gmail.com>
2023-07-02 22:56:41 +0900

    [SP-122] test: 팀 상세 조회 테스트 추가

--- src/test/java/com/cupid/jikting/like/controller/LikeControllerTest.java
+++ src/test/java/com/cupid/jikting/like/controller/LikeControllerTest.java
@@ -6,7 +6,9 @@ import com.cupid.jikting.common.error.ApplicationError;
 import com.cupid.jikting.common.error.ApplicationException;
 import com.cupid.jikting.common.error.NotFoundException;
 import com.cupid.jikting.like.dto.LikeResponse;
+import com.cupid.jikting.like.dto.TeamDetailResponse;
 import com.cupid.jikting.like.service.LikeService;
+import com.cupid.jikting.like.dto.MemberResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -30,13 +32,24 @@ public class LikeControllerTest extends ApiDocument {
     private static final String CONTEXT_PATH = "/api/v1";
     private static final String DOMAIN_ROOT_PATH = "/likes";
     private static final String PATH_DELIMITER = "/";
-    private static final String KEYWORD = "키워드";
     private static final String URL = "http://test-url";
     private static final String NAME = "팀명";
     private static final Long ID = 1L;
+    private static final String NICKNAME = "닉네임";
+    private static final int AGE = 20;
+    private final String MBTI = "mbti";
+    private final String ADDRESS = "거주지";
+    private static final String COMPANY = "회사";
+    private final boolean IS_SMOKE = true;
+    private static final String DRINK_STATUS = "안마심";
+    private static final int HEIGHT = 180;
+    private static final String DESCRIPTION = "소개";
+    private static final String KEYWORD = "키워드";
+    private final String COLLEGE = "대학";
 
     private List<LikeResponse> likeResponses;
     private ApplicationException teamNotFoundException;
+    private TeamDetailResponse teamDetailResponse;
 
     @MockBean
     private LikeService likeService;
@@ -49,6 +62,22 @@ public class LikeControllerTest extends ApiDocument {
         List<String> imageUrls = IntStream.rangeClosed(1, 3)
                 .mapToObj(n -> URL + n)
                 .collect(Collectors.toList());
+        List<MemberResponse> memberResponses = IntStream.rangeClosed(1, 2)
+                .mapToObj(n -> MemberResponse.builder()
+                        .nickname(NICKNAME)
+                        .image(URL)
+                        .age(AGE)
+                        .mbti(MBTI)
+                        .address(ADDRESS)
+                        .company(COMPANY)
+                        .isSmoke(IS_SMOKE)
+                        .drinkStatus(DRINK_STATUS)
+                        .height(HEIGHT)
+                        .description(DESCRIPTION)
+                        .keywords(keywords)
+                        .college(COLLEGE)
+                        .build())
+                .collect(Collectors.toList());
         LikeResponse likeResponse = LikeResponse.builder()
                 .likeId(ID)
                 .name(NAME)
@@ -58,6 +87,12 @@ public class LikeControllerTest extends ApiDocument {
         likeResponses = IntStream.rangeClosed(0, 2)
                 .mapToObj(n -> likeResponse)
                 .collect(Collectors.toList());
+        teamDetailResponse = TeamDetailResponse.builder()
+                .likeId(ID)
+                .teamName(NAME)
+                .keywords(keywords)
+                .members(memberResponses)
+                .build();
         teamNotFoundException = new NotFoundException(ApplicationError.TEAM_NOT_FOUND);
     }
 
@@ -121,6 +156,26 @@ public class LikeControllerTest extends ApiDocument {
         받은_호감_수락_요청_실패(resultActions);
     }
 
+    @Test
+    void 팀_상세_조회_성공() throws Exception {
+        //given
+        willReturn(teamDetailResponse).given(likeService).getTeamDetail(anyLong());
+        //when
+        ResultActions resultActions = 팀_상세_조회_요청();
+        //then
+        팀_상세_조회_요청_성공(resultActions);
+    }
+
+    @Test
+    void 팀_상세_조회_실패() throws Exception {
+        //given
+        willThrow(teamNotFoundException).given(likeService).getTeamDetail(anyLong());
+        //when
+        ResultActions resultActions = 팀_상세_조회_요청();
+        //then
+        팀_상세_조회_요청_실패(resultActions);
+    }
+
     private ResultActions 받은_호감_목록_조회_요청() throws Exception {
         ResultActions resultActions = mockMvc.perform(get(CONTEXT_PATH + DOMAIN_ROOT_PATH + "/received")
                 .contextPath(CONTEXT_PATH));
@@ -178,5 +233,24 @@ public class LikeControllerTest extends ApiDocument {
                         .andExpect(status().isBadRequest())
                         .andExpect(content().json(toJson(ErrorResponse.from(teamNotFoundException)))),
                 "accept-like-fail");
+    }
+
+    private ResultActions 팀_상세_조회_요청() throws Exception {
+        return mockMvc.perform(get(CONTEXT_PATH + DOMAIN_ROOT_PATH + PATH_DELIMITER + ID + "/detail")
+                .contextPath(CONTEXT_PATH));
+    }
+
+    private void 팀_상세_조회_요청_성공(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isOk())
+                        .andExpect(content().json(toJson(teamDetailResponse))),
+                "get-team-detail-success");
+    }
+
+    private void 팀_상세_조회_요청_실패(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().json(toJson(ErrorResponse.from(teamNotFoundException)))),
+                "get-team-detail-fail");
     }
 }
