a72fc8a23053a4621fb12683d031ea56c8a0181c
jeonyuneo <piq2255@gmail.com>
2023-07-03 21:44:00 +0900

    [SP-103] feat: SignUpVerificationCodeRequest 추가

---
+++ src/main/java/com/cupid/jikting/member/dto/SignUpVerificationCodeRequest.java
@@ -0,0 +1,12 @@
+package com.cupid.jikting.member.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignUpVerificationCodeRequest {
+
+    private String phone;
+}
