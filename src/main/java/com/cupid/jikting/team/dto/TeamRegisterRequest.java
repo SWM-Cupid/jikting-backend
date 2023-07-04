7bd42f8d8ff80f8ea7d2210df325b500c4d25c72
jeonyuneo <piq2255@gmail.com>
2023-07-04 15:05:56 +0900

    [SP-32] feat: TeamRegisterRequest 추가

---
+++ src/main/java/com/cupid/jikting/team/dto/TeamRegisterRequest.java
@@ -0,0 +1,15 @@
+package com.cupid.jikting.team.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamRegisterRequest {
+
+    private String description;
+    private List<String> keywords;
+}
