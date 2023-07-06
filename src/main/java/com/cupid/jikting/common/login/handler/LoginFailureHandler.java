fa539a6c98ec6ea98d44096b21b8dbb25955b49d
smc2315 <choeyongtae085@gmail.com>
2023-07-06 17:15:01 +0900

    [SP-136] feat: LoginFailureHandler 추가

---
+++ src/main/java/com/cupid/jikting/common/login/handler/LoginFailureHandler.java
@@ -0,0 +1,24 @@
+package com.cupid.jikting.common.login.handler;
+
+import com.cupid.jikting.common.error.ApplicationError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
+        response.getWriter().write(ApplicationError.NOT_EQUAL_ID_OR_PASSWORD.getMessage());
+        log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
+    }
+}
