616ec1fbd9f6c4629cdc33416ff41762458c5474
smc2315 <choeyongtae085@gmail.com>
2023-07-06 17:15:24 +0900

    [SP-136] feat: Member 추가

---
+++ src/main/java/com/cupid/jikting/member/entity/Member.java
@@ -0,0 +1,45 @@
+package com.cupid.jikting.member.entity;
+
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String phone;
+    private String name;
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    private String socialId;
+
+    private String refreshToken;
+
+    public void authorizeUser() {
+        this.role = Role.UNCERTIFIED;
+    }
+
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
+}
