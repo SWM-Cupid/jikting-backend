18c77541475efa09efe1babb3065f79c30cdb7b5
smc2315 <choeyongtae085@gmail.com>
2023-07-06 17:14:53 +0900

    [SP-136] feat: KakaoOAuth2UserInfo 추가

---
+++ src/main/java/com/cupid/jikting/common/oauth2/userinfo/KakaoOAuth2UserInfo.java
@@ -0,0 +1,15 @@
+package com.cupid.jikting.common.oauth2.userinfo;
+
+import java.util.Map;
+
+public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
+
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return String.valueOf(attributes.get("id"));
+    }
+}
