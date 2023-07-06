0d554bbc98bd4f1d022baa38b7992d2bf89f35f1
smc2315 <choeyongtae085@gmail.com>
2023-07-06 20:24:50 +0900

    [SP-127] refactor: 번개팅 api url 수정

--- src/main/java/com/cupid/jikting/instantmeeting/controller/InstantMeetingController.java
+++ src/main/java/com/cupid/jikting/instantmeeting/controller/InstantMeetingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/instants")
+@RequestMapping("/instant-meetings")
 public class InstantMeetingController {
 
     private final InstantMeetingService instantMeetingService;
