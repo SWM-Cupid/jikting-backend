48c5f57cd569914c1bd5445725ad0b3a5f6a7903
jeonyuneo <piq2255@gmail.com>
2023-07-08 13:19:29 +0900

    [SP-103] test: 전화번호 인증번호 발급 실패 테스트 추가

--- src/test/java/com/cupid/jikting/member/controller/MemberControllerTest.java
+++ src/test/java/com/cupid/jikting/member/controller/MemberControllerTest.java
@@ -461,6 +461,16 @@ public class MemberControllerTest extends ApiDocument {
     }
 
     @Test
+    void 전화번호_인증번호_발급_실패() throws Exception {
+        // given
+        willThrow(wrongFormException).given(memberService).createVerificationCodeForSignup(any(SignUpVerificationCodeRequest.class));
+        // when
+        ResultActions resultActions = 전화번호_인증번호_발급_요청();
+        // then
+        전화번호_인증번호_발급_요청_실패(resultActions);
+    }
+
+    @Test
     void 비밀번호_재설정_인증번호_발급_성공() throws Exception {
         // given
         willDoNothing().given(memberService).createVerificationCodeForResetPassword(any(PasswordResetVerificationCodeRequest.class));
@@ -754,6 +764,13 @@ public class MemberControllerTest extends ApiDocument {
         printAndMakeSnippet(resultActions
                         .andExpect(status().isOk()),
                 "signup-create-verification-code-success");
+    }
+
+    private void 전화번호_인증번호_발급_요청_실패(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().json(toJson(ErrorResponse.from(wrongFormException)))),
+                "signup-create-verification-code-fail");
     }
 
     private ResultActions 아이디_찾기_인증번호_발급_요청() throws Exception {
