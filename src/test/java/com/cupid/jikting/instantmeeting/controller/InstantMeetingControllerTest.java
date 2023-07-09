e4aa83ca77f115389127a9e61be84ea9ecb45f13
smc2315 <choeyongtae085@gmail.com>
2023-07-10 01:13:42 +0900

    [SP-128] test: 번개팅 참여 테스트 추가

--- src/test/java/com/cupid/jikting/instantmeeting/controller/InstantMeetingControllerTest.java
+++ src/test/java/com/cupid/jikting/instantmeeting/controller/InstantMeetingControllerTest.java
@@ -1,6 +1,10 @@
 package com.cupid.jikting.instantmeeting.controller;
 
 import com.cupid.jikting.ApiDocument;
+import com.cupid.jikting.common.dto.ErrorResponse;
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.ApplicationException;
+import com.cupid.jikting.common.error.BadRequestException;
 import com.cupid.jikting.instantmeeting.dto.InstantMeetingResponse;
 import com.cupid.jikting.instantmeeting.service.InstantMeetingService;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +19,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,11 +31,13 @@ public class InstantMeetingControllerTest extends ApiDocument {
 
     private static final String CONTEXT_PATH = "/api/v1";
     private static final String DOMAIN_ROOT_PATH = "/instant-meetings";
+    private static final String PATH_DELIMITER = "/";
     private static final Long ID = 1L;
     private static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2023, Month.JUNE, 30, 18, 0);
     private static final String LOCATION = "장소";
 
     private List<InstantMeetingResponse> instantMeetingResponses;
+    private ApplicationException instantMeetingAlreadyFullException;
 
     @MockBean
     private InstantMeetingService instantMeetingService;
@@ -43,6 +51,7 @@ public class InstantMeetingControllerTest extends ApiDocument {
                         .location(LOCATION)
                         .build())
                 .collect(Collectors.toList());
+        instantMeetingAlreadyFullException = new BadRequestException(ApplicationError.INSTANT_MEETING_ALREADY_FULL);
     }
 
     @Test
@@ -55,6 +64,26 @@ public class InstantMeetingControllerTest extends ApiDocument {
         번개팅_조회_요청_성공(resultActions);
     }
 
+    @Test
+    void 번개팅_참여_성공() throws Exception {
+        //given
+        willDoNothing().given(instantMeetingService).attend(anyLong());
+        //when
+        ResultActions resultActions = 번개팅_참여_요청();
+        //then
+        번개팅_참여_요청_성공(resultActions);
+    }
+
+    @Test
+    void 번개팅_참여_실패() throws Exception {
+        //given
+        willThrow(instantMeetingAlreadyFullException).given(instantMeetingService).attend(anyLong());
+        //when
+        ResultActions resultActions = 번개팅_참여_요청();
+        //then
+        번개팅_참여_요청_실패(resultActions);
+    }
+
     private ResultActions 번개팅_조회_요청() throws Exception {
         return mockMvc.perform(get(CONTEXT_PATH + DOMAIN_ROOT_PATH)
                 .contextPath(CONTEXT_PATH));
@@ -65,5 +94,23 @@ public class InstantMeetingControllerTest extends ApiDocument {
                         .andExpect(status().isOk())
                         .andExpect(content().json(toJson(instantMeetingResponses))),
                 "get-instant-meeting-success");
+    }
+
+    private ResultActions 번개팅_참여_요청() throws Exception {
+        return mockMvc.perform(post(CONTEXT_PATH + DOMAIN_ROOT_PATH + PATH_DELIMITER + ID)
+                .contextPath(CONTEXT_PATH));
+    }
+
+    private void 번개팅_참여_요청_성공(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isOk()),
+                "attend-instant-meeting-success");
+    }
+
+    private void 번개팅_참여_요청_실패(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().json(toJson(ErrorResponse.from(instantMeetingAlreadyFullException)))),
+                "attend-instant-meeting-fail");
     }
 }
