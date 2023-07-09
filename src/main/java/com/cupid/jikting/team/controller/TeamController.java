d61afcb39204950543e1be1be0f1d4e6a597dd9a
smc2315 <choeyongtae085@gmail.com>
2023-07-10 01:52:48 +0900

    [SP-36] feat: 팀 조회 추가

--- src/main/java/com/cupid/jikting/team/controller/TeamController.java
+++ src/main/java/com/cupid/jikting/team/controller/TeamController.java
@@ -2,13 +2,11 @@ package com.cupid.jikting.team.controller;
 
 import com.cupid.jikting.team.dto.TeamRegisterRequest;
 import com.cupid.jikting.team.dto.TeamRegisterResponse;
+import com.cupid.jikting.team.dto.TeamResponse;
 import com.cupid.jikting.team.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +18,10 @@ public class TeamController {
     @PostMapping
     public ResponseEntity<TeamRegisterResponse> register(@RequestBody TeamRegisterRequest teamRegisterRequest) {
         return ResponseEntity.ok().body(teamService.register(teamRegisterRequest));
+    }
+
+    @GetMapping("/{teamId}")
+    public ResponseEntity<TeamResponse> get(@PathVariable Long teamId) {
+        return ResponseEntity.ok().body(teamService.get(teamId));
     }
 }
