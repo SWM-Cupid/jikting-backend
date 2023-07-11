9b1e9e3d4f60e6ebac9fbb70343e1c68b562df85
smc2315 <choeyongtae085@gmail.com>
2023-07-12 00:12:10 +0900

    [SP-125] test: 미팅 확정 테스트 추가

--- src/test/java/com/cupid/jikting/chatting/controller/ChattingControllerTest.java
+++ src/test/java/com/cupid/jikting/chatting/controller/ChattingControllerTest.java
@@ -2,19 +2,29 @@ package com.cupid.jikting.chatting.controller;
 
 import com.cupid.jikting.ApiDocument;
 import com.cupid.jikting.chatting.dto.ChattingResponse;
+import com.cupid.jikting.chatting.dto.MeetingConfirmRequest;
 import com.cupid.jikting.chatting.service.ChattingService;
+import com.cupid.jikting.common.dto.ErrorResponse;
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.ApplicationException;
+import com.cupid.jikting.common.error.WrongFormException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,12 +33,17 @@ public class ChattingControllerTest extends ApiDocument {
 
     private static final String CONTEXT_PATH = "/api/v1";
     private static final String DOMAIN_ROOT_PATH = "/chattings";
+    private static final String PATH_DELIMITER = "/";
     private static final String URL = "이미지 주소";
     private static final Long ID = 1L;
+    private static final String PLACE = "미팅장소";
+    private static final LocalDateTime DATE = LocalDateTime.of(2023, Month.JUNE, 30, 18, 0);
     private static final String TEAM_NAME = "팀이름";
     private static final String MESSAGE = "메시지";
 
+    private MeetingConfirmRequest meetingConfirmRequest;
     private List<ChattingResponse> chattingResponses;
+    private ApplicationException wrongFormException;
 
     @MockBean
     private ChattingService chattingService;
@@ -38,14 +53,20 @@ public class ChattingControllerTest extends ApiDocument {
         List<String> images = IntStream.rangeClosed(0, 2)
                 .mapToObj(n -> URL)
                 .collect(Collectors.toList());
+        meetingConfirmRequest = MeetingConfirmRequest.builder()
+                .meetingId(ID)
+                .place(PLACE)
+                .date(DATE)
+                .build();
         chattingResponses = IntStream.rangeClosed(0, 2)
                 .mapToObj(n -> ChattingResponse.builder()
-                        .chattingId(ID)
+                        .chattingRoomId(ID)
                         .opposingTeamName(TEAM_NAME)
                         .lastMessage(MESSAGE)
                         .images(images)
                         .build())
                 .collect(Collectors.toList());
+        wrongFormException = new WrongFormException(ApplicationError.INVALID_FORMAT);
     }
 
     @Test
@@ -58,6 +79,26 @@ public class ChattingControllerTest extends ApiDocument {
         채팅방_목록_조회_요청_성공(resultActions);
     }
 
+    @Test
+    void 미팅_확정_성공() throws Exception {
+        //given
+        willDoNothing().given(chattingService).confirm(anyLong());
+        //when
+        ResultActions resultActions = 미팅_확정_요청();
+        //then
+        미팅_확정_요쳥_성공(resultActions);
+    }
+
+    @Test
+    void 미팅_확정_실패() throws Exception {
+        //given
+        willThrow(wrongFormException).given(chattingService).confirm(anyLong());
+        //when
+        ResultActions resultActions = 미팅_확정_요청();
+        //then
+        미팅_확정_요청_실패(resultActions);
+    }
+
     private ResultActions 채팅방_목록_조회_요청() throws Exception {
         return mockMvc.perform(get(CONTEXT_PATH + DOMAIN_ROOT_PATH)
                 .contextPath(CONTEXT_PATH));
@@ -68,5 +109,25 @@ public class ChattingControllerTest extends ApiDocument {
                         .andExpect(status().isOk())
                         .andExpect(content().json(toJson(chattingResponses))),
                 "get-chattings-success");
+    }
+
+    private ResultActions 미팅_확정_요청() throws Exception {
+        return mockMvc.perform(post(CONTEXT_PATH + DOMAIN_ROOT_PATH + PATH_DELIMITER + ID + "/confirm")
+                .contextPath(CONTEXT_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(meetingConfirmRequest)));
+    }
+
+    private void 미팅_확정_요쳥_성공(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isOk())
+                , "meeting-confirm-success");
+    }
+
+    private void 미팅_확정_요청_실패(ResultActions resultActions) throws Exception {
+        printAndMakeSnippet(resultActions
+                        .andExpect(status().isBadRequest())
+                        .andExpect(content().json(toJson(ErrorResponse.from(wrongFormException)))),
+                "meeting-confirm-fail");
     }
 }
