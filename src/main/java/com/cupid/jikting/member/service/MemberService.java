7427466b04e3b3152089a4dcf3102bacd3f34e7b
smc2315 <choeyongtae085@gmail.com>
2023-07-13 16:21:05 +0900

    [SP-136] refactor: 필요없는 annotation 제거

--- src/main/java/com/cupid/jikting/member/service/MemberService.java
+++ src/main/java/com/cupid/jikting/member/service/MemberService.java
@@ -1,14 +1,9 @@
 package com.cupid.jikting.member.service;
 
 import com.cupid.jikting.member.dto.*;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
-
-@RequiredArgsConstructor
-@Transactional
 @Service
 public class MemberService {
 
