2cafe368c604dd4b1d5a3ec3611e6508aee80076
smc2315 <choeyongtae085@gmail.com>
2023-07-13 16:08:40 +0900

    [SP-136] feat: ExceptionHandlerFilter 추가

---
+++ src/main/java/com/cupid/jikting/common/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,48 @@
+package com.cupid.jikting.common.filter;
+
+import com.cupid.jikting.common.dto.ErrorResponse;
+import com.cupid.jikting.common.error.ApplicationException;
+import com.cupid.jikting.common.error.ExpiredJwtException;
+import com.cupid.jikting.common.error.InvalidJwtException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    private static final String ENCODING = "UTF-8";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException | InvalidJwtException exception) {
+            setErrorResponse(response, exception);
+        }
+    }
+
+    private void setErrorResponse(
+            HttpServletResponse response,
+            ApplicationException exception
+    ) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(exception.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(ENCODING);
+        try {
+            response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.from(exception)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
