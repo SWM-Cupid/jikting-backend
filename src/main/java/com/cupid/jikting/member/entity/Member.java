417232c22f70185f707431c3cc1fada3b9252573
smc2315 <choeyongtae085@gmail.com>
2023-07-13 16:20:18 +0900

    [SP-136] style: annotation 달린 필드 앞뒤 개행 추가

--- src/main/java/com/cupid/jikting/member/entity/Member.java
+++ src/main/java/com/cupid/jikting/member/entity/Member.java
@@ -15,6 +15,7 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String password;
     private String phone;
