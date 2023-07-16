8937863e182741f670b89440d6b5ec57657ba370
jeonyuneo <piq2255@gmail.com>
2023-07-16 23:22:27 +0700

    [SP-139] feat: Meeting 추가

---
+++ src/main/java/com/cupid/jikting/meeting/entity/Meeting.java
@@ -0,0 +1,34 @@
+package com.cupid.jikting.meeting.entity;
+
+import com.cupid.jikting.common.entity.BaseEntity;
+import com.cupid.jikting.team.entity.Team;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE meeting SET is_deleted = true WHERE meeting_id = ?")
+@AttributeOverride(name = "id", column = @Column(name = "meeting_id"))
+@Entity
+public class Meeting extends BaseEntity {
+
+    private LocalDateTime schedule;
+    private String place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recommended_team_id")
+    private Team recommendedTeam;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "acceptiong_team_id")
+    private Team acceptingTeam;
+}
