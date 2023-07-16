396ddd45e93143ec6563fb4a647967daa42d2924
jeonyuneo <piq2255@gmail.com>
2023-07-16 23:13:16 +0700

    [SP-139] feat: Hobby 추가

---
+++ src/main/java/com/cupid/jikting/common/entity/Hobby.java
@@ -0,0 +1,29 @@
+package com.cupid.jikting.common.entity;
+
+import com.cupid.jikting.member.entity.MemberHobby;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE hobby SET is_deleted = true WHERE hobby_id = ?")
+@AttributeOverride(name = "id", column = @Column(name = "hobby_id"))
+@Entity
+public class Hobby extends BaseEntity {
+
+    private String keyword;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "hobby")
+    private final List<MemberHobby> memberHobbies = new ArrayList<>();
+}
