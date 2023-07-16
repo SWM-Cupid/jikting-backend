791da4fb7fb312c53b7ec0dd4780763460acab53
jeonyuneo <piq2255@gmail.com>
2023-07-16 23:01:13 +0700

    [SP-139] feat: InstantMeeting 추가

---
+++ src/main/java/com/cupid/jikting/meeting/entity/InstantMeeting.java
@@ -0,0 +1,32 @@
+package com.cupid.jikting.meeting.entity;
+
+import com.cupid.jikting.common.entity.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE instant_meeting SET is_deleted = true WHERE instant_meeting_id = ?")
+@AttributeOverride(name = "id", column = @Column(name = "instant_meeting_id"))
+@Entity
+public class InstantMeeting extends BaseEntity {
+
+    private LocalDateTime schedule;
+    private String place;
+    private int memberCount;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "instantMeeting")
+    private final List<InstantMeetingMember> instantMeetingMembers = new ArrayList<>();
+}
