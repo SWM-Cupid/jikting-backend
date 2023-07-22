444f15d4a4e84495d7b621b2e6adf90b914550c8
jeonyuneo <piq2255@gmail.com>
2023-07-23 03:37:56 +0900

    [SP-157] feat: ChattingRoomController 루트 경로 수정

--- src/main/java/com/cupid/jikting/chatting/controller/ChattingRoomController.java
+++ src/main/java/com/cupid/jikting/chatting/controller/ChattingRoomController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/chattings/room")
+@RequestMapping("/chattings/rooms")
 public class ChattingRoomController {
 
     private final ChattingRoomService chattingRoomService;
