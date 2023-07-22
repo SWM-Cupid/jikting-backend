0f763f59bca9344e5d07b459c39817049eb73853
jeonyuneo <piq2255@gmail.com>
2023-07-22 20:42:40 +0900

    [SP-157] feat: ChattingService 채팅 메시지 전송 기능 추가

--- src/main/java/com/cupid/jikting/chatting/service/ChattingService.java
+++ src/main/java/com/cupid/jikting/chatting/service/ChattingService.java
@@ -1,22 +1,10 @@
 package com.cupid.jikting.chatting.service;
 
-import com.cupid.jikting.chatting.dto.ChattingRoomDetailResponse;
-import com.cupid.jikting.chatting.dto.ChattingRoomResponse;
+import com.cupid.jikting.chatting.dto.ChattingRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class ChattingService {
+public interface ChattingService {
 
-    public List<ChattingRoomResponse> getAll() {
-        return null;
-    }
-
-    public ChattingRoomDetailResponse get(Long chattingRoomId) {
-        return null;
-    }
-
-    public void confirm(Long chattingRoomId) {
-    }
+    void sendMessage(Long chattingRoomId, ChattingRequest chattingRequest);
 }
