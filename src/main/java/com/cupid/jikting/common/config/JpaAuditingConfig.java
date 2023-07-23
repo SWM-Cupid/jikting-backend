da9a56495177b88c45fe9cb7ff24f470b78f7d7a
jeonyuneo <piq2255@gmail.com>
2023-07-24 01:38:23 +0900

    [SP-159] refactor: JpaAuditingConfig 패키지 이동

--- src/main/java/com/cupid/jikting/common/config/JpaAuditingConfig.java
+++ src/main/java/com/cupid/jikting/common/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.cupid.jikting.config;
+package com.cupid.jikting.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
