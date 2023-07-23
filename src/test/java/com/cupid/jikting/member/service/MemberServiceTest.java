2f0617ed5fffd16548c7734b19d67785f0706894
jeonyuneo <piq2255@gmail.com>
2023-07-24 02:21:19 +0900

    [SP-159] test: MemberServiceTest 아이디 중복 확인 실패 테스트 추가

--- src/test/java/com/cupid/jikting/member/service/MemberServiceTest.java
+++ src/test/java/com/cupid/jikting/member/service/MemberServiceTest.java
@@ -1,5 +1,7 @@
 package com.cupid.jikting.member.service;
 
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.DuplicateException;
 import com.cupid.jikting.member.dto.UsernameCheckRequest;
 import com.cupid.jikting.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.verify;
@@ -41,5 +44,15 @@ class MemberServiceTest {
         memberService.checkDuplicatedUsername(usernameCheckRequest);
         // then
         verify(memberRepository).existsByUsername(anyString());
+    }
+
+    @Test
+    void 아이디_중복_확인_실패_존재하는_아이디() {
+        // given
+        willReturn(true).given(memberRepository).existsByUsername(anyString());
+        // when & then
+        assertThatThrownBy(() -> memberService.checkDuplicatedUsername(usernameCheckRequest))
+                .isInstanceOf(DuplicateException.class)
+                .hasMessage(ApplicationError.DUPLICATE_USERNAME.getMessage());
     }
 }
