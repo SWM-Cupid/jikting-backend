c003338c171f577a04a6a0922df1340ace2f1a53
jeonyuneo <piq2255@gmail.com>
2023-07-24 02:22:06 +0900

    [SP-159] refactor: MemberRepository findByUsername 메소드 파라미터명 수정

--- src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
+++ src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByUsername(String email);
+    Optional<Member> findByUsername(String username);
 
     Optional<Member> findByRefreshToken(String refreshToken);
 
