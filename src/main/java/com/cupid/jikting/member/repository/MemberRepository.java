f8fb590067ddd51914554647f4a230d9617cc22c
jeonyuneo <piq2255@gmail.com>
2023-07-24 12:39:08 +0900

    [SP-160] feat: MemberRepository 닉네임 존재 여부 확인 메소드 추가

--- src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
+++ src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
@@ -12,4 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByRefreshToken(String refreshToken);
 
     boolean existsByUsername(String username);
+
+    boolean existsByNickname(String nickname);
 }
