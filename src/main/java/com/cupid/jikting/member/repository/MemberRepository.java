c9a6818ed74b551a49858504284362f3e6799a99
smc2315 <choeyongtae085@gmail.com>
2023-07-24 13:13:37 +0900

    [SP-138] fix: 미사용 매서드 제거

--- src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
+++ src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
@@ -10,6 +10,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String email);
 
     Optional<Member> findByRefreshToken(String refreshToken);
-
-    boolean existsByUsername(String username);
 }
