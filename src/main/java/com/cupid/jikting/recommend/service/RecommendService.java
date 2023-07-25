602df2ae7886f122f49ceb78e1c38834cbb9fed8
smc2315 <choeyongtae085@gmail.com>
2023-07-25 21:41:43 +0900

    [SP-208] feat: 추천 팀 조회 로직 추가

--- src/main/java/com/cupid/jikting/recommend/service/RecommendService.java
+++ src/main/java/com/cupid/jikting/recommend/service/RecommendService.java
@@ -1,17 +1,93 @@
 package com.cupid.jikting.recommend.service;
 
+import com.cupid.jikting.common.entity.Hobby;
+import com.cupid.jikting.common.entity.Personality;
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.NotFoundException;
+import com.cupid.jikting.member.entity.MemberHobby;
+import com.cupid.jikting.member.entity.MemberPersonality;
+import com.cupid.jikting.member.entity.MemberProfile;
+import com.cupid.jikting.member.repository.MemberProfileRepository;
+import com.cupid.jikting.recommend.dto.ImageResponse;
+import com.cupid.jikting.recommend.dto.MemberResponse;
 import com.cupid.jikting.recommend.dto.RecommendResponse;
+import com.cupid.jikting.recommend.entity.Recommend;
+import com.cupid.jikting.team.entity.TeamPersonality;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Service
 public class RecommendService {
 
-    public List<RecommendResponse> get() {
-        return null;
+    private final MemberProfileRepository memberProfileRepository;
+
+    public List<RecommendResponse> get(Long memberProfileId) {
+        MemberProfile memberProfile = memberProfileRepository.findById(memberProfileId)
+                .orElseThrow(() -> new NotFoundException(ApplicationError.MEMBER_NOT_FOUND));
+        return memberProfile.getRecommends()
+                .stream()
+                .map(recommend -> RecommendResponse.builder()
+                        .recommendId(recommend.getId())
+                        .members(getMemberResponses(recommend))
+                        .personalities(getTeamPersonalities(recommend))
+                        .build())
+                .collect(Collectors.toList());
     }
 
     public void sendLike(Long recommendId) {
+    }
+
+    private static List<MemberResponse> getMemberResponses(Recommend recommend) {
+        return recommend.getFrom()
+                .getMemberProfiles()
+                .stream()
+                .map(RecommendService::toMemberResponse)
+                .collect(Collectors.toList());
+    }
+
+    private static MemberResponse toMemberResponse(MemberProfile memberProfile) {
+        return MemberResponse.builder()
+                .nickname(memberProfile.getNickname())
+                .images(List.of(ImageResponse.builder().build())) // TODO
+                .age(memberProfile.getAge())
+                .mbti(memberProfile.getMbti().toString())
+                .address(memberProfile.getAddress())
+                .company(memberProfile.getCompany())
+                .smokeStatus(memberProfile.getSmokeStatus().getValue())
+                .drinkStatus(memberProfile.getDrinkStatus().getValue())
+                .height(memberProfile.getHeight())
+                .description(memberProfile.getDescription())
+                .personalities(getMemberProfilePersonalities(memberProfile))
+                .hobbies(getHobbies(memberProfile))
+                .college(memberProfile.getCollege())
+                .build();
+    }
+
+
+    private static List<String> getMemberProfilePersonalities(MemberProfile memberProfile) {
+        return memberProfile.getMemberPersonalities().stream()
+                .map(MemberPersonality::getPersonality)
+                .map(Personality::getKeyword)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> getHobbies(MemberProfile memberProfile) {
+        return memberProfile.getMemberHobbies().stream()
+                .map(MemberHobby::getHobby)
+                .map(Hobby::getKeyword)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> getTeamPersonalities(Recommend recommend) {
+        return recommend.getFrom()
+                .getTeamPersonalities()
+                .stream()
+                .map(TeamPersonality::getPersonality)
+                .map(Personality::getKeyword)
+                .collect(Collectors.toList());
     }
 }
