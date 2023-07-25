91ba30a038fcc03ea7b5a14e4569814a326a7055
jeonyuneo <piq2255@gmail.com>
2023-07-25 15:44:12 +0900

    [SP-158] fix: StompChattingService Member 관련 필드 MemberProfile로 수정

--- src/main/java/com/cupid/jikting/chatting/service/StompChattingService.java
+++ src/main/java/com/cupid/jikting/chatting/service/StompChattingService.java
@@ -6,8 +6,8 @@ import com.cupid.jikting.chatting.entity.ChattingRoom;
 import com.cupid.jikting.chatting.repository.ChattingRoomRepository;
 import com.cupid.jikting.common.error.ApplicationError;
 import com.cupid.jikting.common.error.NotFoundException;
-import com.cupid.jikting.member.entity.Member;
-import com.cupid.jikting.member.repository.MemberRepository;
+import com.cupid.jikting.member.entity.MemberProfile;
+import com.cupid.jikting.member.repository.MemberProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,17 +16,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class StompChattingService implements ChattingService {
 
-    private final MemberRepository memberRepository;
+    private final MemberProfileRepository memberProfileRepository;
     private final ChattingRoomRepository chattingRoomRepository;
 
     @Transactional
     public void sendMessage(Long chattingRoomId, ChattingRequest chattingRequest) {
-        Member sender = memberRepository.findById(chattingRequest.getSenderId())
+        MemberProfile sender = memberProfileRepository.findById(chattingRequest.getSenderId())
                 .orElseThrow(() -> new NotFoundException(ApplicationError.MEMBER_NOT_FOUND));
         ChattingRoom chattingRoom = chattingRoomRepository.findById(chattingRoomId)
                 .orElseThrow(() -> new NotFoundException(ApplicationError.CHATTING_ROOM_NOT_FOUND));
         Chatting chatting = Chatting.builder()
-                .member(sender)
+                .memberProfile(sender)
                 .chattingRoom(chattingRoom)
                 .content(chattingRequest.getContent())
                 .build();
