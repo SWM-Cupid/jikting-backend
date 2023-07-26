5e00af208ef9c216e5be2d4be694893182494eff
jeonyuneo <piq2255@gmail.com>
2023-07-26 17:14:35 +0900

    [SP-186] feat: TeamService 팀 등록 기능 추가

--- src/main/java/com/cupid/jikting/team/service/TeamService.java
+++ src/main/java/com/cupid/jikting/team/service/TeamService.java
@@ -1,16 +1,41 @@
 package com.cupid.jikting.team.service;
 
+import com.cupid.jikting.common.entity.Personality;
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.BadRequestException;
 import com.cupid.jikting.team.dto.TeamRegisterRequest;
 import com.cupid.jikting.team.dto.TeamRegisterResponse;
 import com.cupid.jikting.team.dto.TeamResponse;
 import com.cupid.jikting.team.dto.TeamUpdateRequest;
+import com.cupid.jikting.team.entity.Team;
+import com.cupid.jikting.team.repository.PersonalityRepository;
+import com.cupid.jikting.team.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class TeamService {
 
+    private static final String TEAM_URL = "https://jikting.com/teams/";
+    private static final String INVITE = "/invite";
+
+    private final TeamRepository teamRepository;
+    private final PersonalityRepository personalityRepository;
+
     public TeamRegisterResponse register(TeamRegisterRequest teamRegisterRequest) {
-        return null;
+        Team team = Team.builder()
+                .name(String.valueOf(UUID.randomUUID()))
+                .description(teamRegisterRequest.getDescription())
+                .memberCount(teamRegisterRequest.getMemberCount())
+                .build();
+        team.addTeamPersonalities(getPersonalities(teamRegisterRequest.getKeywords()));
+        Team savedTeam = teamRepository.save(team);
+        return TeamRegisterResponse.from(TEAM_URL + savedTeam.getId() + INVITE);
     }
 
     public void attend(Long teamId) {
@@ -27,5 +52,16 @@ public class TeamService {
     }
 
     public void deleteMember(Long teamId, Long memberId) {
+    }
+
+    private List<Personality> getPersonalities(List<String> keywords) {
+        return keywords.stream()
+                .map(this::getPersonality)
+                .collect(Collectors.toList());
+    }
+
+    private Personality getPersonality(String keyword) {
+        return personalityRepository.findByKeyword(keyword)
+                .orElseThrow(() -> new BadRequestException(ApplicationError.PERSONALITY_NOT_FOUND));
     }
 }
