925f4b0db59245a57d50c9a86f5c42d1e68348ac
jeonyuneo <piq2255@gmail.com>
2023-07-26 17:24:04 +0900

    [SP-186] feat: PersonalityRepository 추가

---
+++ src/main/java/com/cupid/jikting/team/repository/PersonalityRepository.java
@@ -0,0 +1,8 @@
+package com.cupid.jikting.team.repository;
+
+import com.cupid.jikting.common.entity.Personality;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonalityRepository extends JpaRepository<Personality, Long> {
+
+}
