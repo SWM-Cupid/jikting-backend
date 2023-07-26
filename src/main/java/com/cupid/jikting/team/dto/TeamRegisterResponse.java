7fbbb574c673d5003d501cc211311658a51d4a84
jeonyuneo <piq2255@gmail.com>
2023-07-26 17:16:37 +0900

    [SP-186] refactor: TeamRegisterResponse 정적 팩토리 메소드 적용

--- src/main/java/com/cupid/jikting/team/dto/TeamRegisterResponse.java
+++ src/main/java/com/cupid/jikting/team/dto/TeamRegisterResponse.java
@@ -3,10 +3,13 @@ package com.cupid.jikting.team.dto;
 import lombok.*;
 
 @Getter
-@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TeamRegisterResponse {
 
     private String invitationUrl;
+
+    public static TeamRegisterResponse from(String invitationUrl) {
+        return new TeamRegisterResponse(invitationUrl);
+    }
 }
