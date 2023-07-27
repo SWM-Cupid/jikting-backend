230b700e80e45ae7b189e0248b62ad3e6bf4df9d
smc2315 <choeyongtae085@gmail.com>
2023-07-27 16:19:23 +0900

    [SP-156] refactor: PasswordUtil chatSet 상수로 변경

--- src/main/java/com/cupid/jikting/common/util/PasswordUtil.java
+++ src/main/java/com/cupid/jikting/common/util/PasswordUtil.java
@@ -5,23 +5,22 @@ import java.util.stream.IntStream;
 
 public class PasswordUtil {
 
+    public static final char[] CHAT_SET = new char[]{
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
     public static String generateRandomPassword() {
-        char[] charSet = new char[]{
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        };
-
-        StringBuffer password = new StringBuffer();
+        StringBuilder password = new StringBuilder();
         Random random = new Random();
-
         IntStream.range(0, 8)
                 .mapToObj(i -> {
                     double randomValue = random.nextDouble();
-                    int index = (int) (charSet.length * randomValue);
-                    return charSet[index];
+                    int index = (int) (CHAT_SET.length * randomValue);
+                    return CHAT_SET[index];
                 })
                 .forEach(password::append);
         return password.toString();
