25298a4c877f511aed11c632470873df268842e6
smc2315 <choeyongtae085@gmail.com>
2023-07-27 13:50:33 +0900

    [SP-156] feat: oauth user 추가

---
+++ src/main/java/com/cupid/jikting/member/oauth2/CustomOAuth2User.java
@@ -0,0 +1,23 @@
+package com.cupid.jikting.member.oauth2;
+
+import com.cupid.jikting.member.entity.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    private final String username;
+    private final Role role;
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey, String username,
+                            Role role) {
+        super(authorities, attributes, nameAttributeKey);
+        this.username = username;
+        this.role = role;
+    }
+}
