d29e480dd78b117974063d3d46fecc3ae7fa6082
jeonyuneo <piq2255@gmail.com>
2023-07-27 17:52:03 +0900

    [SP-188] test: TeamServiceTest 팀 조회 성공 테스트 추가

--- src/test/java/com/cupid/jikting/team/service/TeamServiceTest.java
+++ src/test/java/com/cupid/jikting/team/service/TeamServiceTest.java
@@ -3,9 +3,13 @@ package com.cupid.jikting.team.service;
 import com.cupid.jikting.common.entity.Personality;
 import com.cupid.jikting.common.error.ApplicationError;
 import com.cupid.jikting.common.error.NotFoundException;
+import com.cupid.jikting.member.entity.Mbti;
+import com.cupid.jikting.member.entity.MemberProfile;
 import com.cupid.jikting.team.dto.TeamRegisterRequest;
 import com.cupid.jikting.team.dto.TeamRegisterResponse;
+import com.cupid.jikting.team.dto.TeamResponse;
 import com.cupid.jikting.team.entity.Team;
+import com.cupid.jikting.team.entity.TeamMember;
 import com.cupid.jikting.team.repository.PersonalityRepository;
 import com.cupid.jikting.team.repository.TeamRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.RollbackException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,8 +29,7 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.verify;
@@ -38,9 +42,14 @@ class TeamServiceTest {
     private static final String NAME = "이름";
     private static final String DESCRIPTION = "한줄소개";
     private static final int MEMBER_COUNT = 3;
+    private static final boolean LEADER = true;
     private static final String INVITATION_URL = "https://jikting.com/teams/" + ID + "/invite";
+    private static final LocalDate BIRTH = LocalDate.of(1996, 5, 10);
+    private static final Mbti MBTI = Mbti.ENFJ;
+    private static final String ADDRESS = "서울시 강남구 테헤란로";
 
     private Personality personality;
+    private List<Personality> personalities;
     private Team team;
     private TeamRegisterRequest teamRegisterRequest;
 
@@ -55,12 +64,12 @@ class TeamServiceTest {
 
     @BeforeEach
     void setUp() {
-        List<Personality> personalities = IntStream.range(0, 3)
-                .mapToObj(n -> personality)
-                .collect(Collectors.toList());
         personality = Personality.builder()
                 .keyword(KEYWORD)
                 .build();
+        personalities = IntStream.range(0, 3)
+                .mapToObj(n -> personality)
+                .collect(Collectors.toList());
         team = Team.builder()
                 .id(ID)
                 .name(NAME)
@@ -68,6 +77,20 @@ class TeamServiceTest {
                 .memberCount(MEMBER_COUNT)
                 .build();
         team.addTeamPersonalities(personalities);
+        MemberProfile leader = MemberProfile.builder()
+                .id(ID)
+                .birth(BIRTH)
+                .mbti(MBTI)
+                .address(ADDRESS)
+                .build();
+        MemberProfile member = MemberProfile.builder()
+                .id(ID)
+                .birth(BIRTH)
+                .mbti(MBTI)
+                .address(ADDRESS)
+                .build();
+        TeamMember.of(LEADER, team, leader);
+        TeamMember.of(!LEADER, team, member);
         teamRegisterRequest = TeamRegisterRequest.builder()
                 .description(DESCRIPTION)
                 .memberCount(MEMBER_COUNT)
@@ -108,5 +131,20 @@ class TeamServiceTest {
         // when & then
         assertThatThrownBy(() -> teamService.register(teamRegisterRequest))
                 .isInstanceOf(RollbackException.class);
+    }
+
+    @Test
+    void 팀_조회_성공() {
+        // given
+        willReturn(Optional.of(team)).given(teamRepository).findById(anyLong());
+        // when
+        TeamResponse teamResponse = teamService.get(ID);
+        // then
+        assertAll(
+                () -> verify(teamRepository).findById(anyLong()),
+                () -> assertThat(teamResponse.getDescription()).isEqualTo(DESCRIPTION),
+                () -> assertThat(teamResponse.getKeywords().size()).isEqualTo(personalities.size()),
+                () -> assertThat(teamResponse.getMembers().size()).isEqualTo(team.getTeamMembers().size())
+        );
     }
 }
