5e839c4a0fb6f9811df0a6ce41c8ab483420ad44
smc2315 <choeyongtae085@gmail.com>
2023-07-27 11:13:12 +0900

    [SP-222] feat: LikeService 받은 호감 조회 로직 추가

--- src/main/java/com/cupid/jikting/like/service/LikeService.java
+++ src/main/java/com/cupid/jikting/like/service/LikeService.java
@@ -1,15 +1,27 @@
 package com.cupid.jikting.like.service;
 
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.BadRequestException;
 import com.cupid.jikting.like.dto.LikeResponse;
 import com.cupid.jikting.like.dto.TeamDetailResponse;
+import com.cupid.jikting.team.repository.TeamMemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class LikeService {
 
-    public List<LikeResponse> getAllReceivedLike() {
+    private final TeamMemberRepository teamMemberRepository;
+
+    public List<LikeResponse> getAllReceivedLike(Long memberProfileId) {
+        Long teamId = teamMemberRepository.getTeamIdByMemberProfileId(memberProfileId)
+                .orElseThrow(() -> new BadRequestException(ApplicationError.NOT_EXIST_REGISTERED_TEAM));
+        log.info("-----------teamID: " + teamId);
         return null;
     }
 
