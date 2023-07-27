e448ade1efe558f7ce54e4e7b46d21a24696fe33
jeonyuneo <piq2255@gmail.com>
2023-07-27 17:41:41 +0900

    [SP-188] refactor: TeamResponse 빌더 패턴 삭제 및 정적 팩토리 메소드 적용

--- src/main/java/com/cupid/jikting/team/dto/TeamResponse.java
+++ src/main/java/com/cupid/jikting/team/dto/TeamResponse.java
@@ -1,11 +1,16 @@
 package com.cupid.jikting.team.dto;
 
-import lombok.*;
+import com.cupid.jikting.team.entity.Team;
+import com.cupid.jikting.team.entity.TeamMember;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TeamResponse {
@@ -13,4 +18,16 @@ public class TeamResponse {
     private String description;
     private List<String> keywords;
     private List<MemberResponse> members;
+
+    public static TeamResponse from(Team team) {
+        List<MemberResponse> memberResponses = team.getTeamMembers()
+                .stream()
+                .map(TeamMember::getMemberProfile)
+                .map(MemberResponse::from)
+                .collect(Collectors.toList());
+        return new TeamResponse(
+                team.getDescription(),
+                team.getPersonalities(),
+                memberResponses);
+    }
 }
