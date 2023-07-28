bcee8fefe5f2ec4104c2976b7e853087bc478f4b
smc2315 <choeyongtae085@gmail.com>
2023-07-28 18:38:14 +0900

    [SP-222] style: 개행 추가

--- src/main/java/com/cupid/jikting/team/repository/TeamLikeRepository.java
+++ src/main/java/com/cupid/jikting/team/repository/TeamLikeRepository.java
@@ -4,4 +4,5 @@ import com.cupid.jikting.team.entity.TeamLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamLikeRepository extends JpaRepository<TeamLike, Long> {
+
 }
