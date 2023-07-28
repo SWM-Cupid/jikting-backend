52508cfebfec0effc29dca9bd58317065e4ad8ab
jeongyuneo <piq2255@gmail.com>
2023-07-29 00:55:27 +0900

    [SP-199] feat: SmokeStatus 메시지로 열거 상수 반환 메소드 추가

--- src/main/java/com/cupid/jikting/member/entity/SmokeStatus.java
+++ src/main/java/com/cupid/jikting/member/entity/SmokeStatus.java
@@ -1,7 +1,11 @@
 package com.cupid.jikting.member.entity;
 
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.BadRequestException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -11,4 +15,11 @@ public enum SmokeStatus {
     SMOKING("흡연");
 
     private final String value;
+
+    public static SmokeStatus find(String message) {
+        return Arrays.stream(values())
+                .filter(value -> value.message.equals(message))
+                .findAny()
+                .orElseThrow(() -> new BadRequestException(ApplicationError.INVALID_GENDER));
+    }
 }
