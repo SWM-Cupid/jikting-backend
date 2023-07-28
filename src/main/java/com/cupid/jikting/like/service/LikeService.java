64bde698955ee734f44ccc7d1a84302720e5e9ea
smc2315 <choeyongtae085@gmail.com>
2023-07-28 18:37:51 +0900

    [SP-222] refactor: @Transactional 추가 및 getTeamMemberById 메서드 추출

--- src/main/java/com/cupid/jikting/like/service/LikeService.java
+++ src/main/java/com/cupid/jikting/like/service/LikeService.java
@@ -4,29 +4,27 @@ import com.cupid.jikting.common.error.ApplicationError;
 import com.cupid.jikting.common.error.BadRequestException;
 import com.cupid.jikting.like.dto.LikeResponse;
 import com.cupid.jikting.like.dto.TeamDetailResponse;
-import com.cupid.jikting.team.entity.TeamLike;
 import com.cupid.jikting.team.entity.TeamMember;
-import com.cupid.jikting.team.repository.TeamLikeRepository;
 import com.cupid.jikting.team.repository.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class LikeService {
 
     private final TeamMemberRepository teamMemberRepository;
 
+    @Transactional(readOnly=true)
     public List<LikeResponse> getAllReceivedLike(Long memberProfileId) {
-        TeamMember teamMember = teamMemberRepository.getTeamMemberByMemberProfileId(memberProfileId)
-                .orElseThrow(() -> new BadRequestException(ApplicationError.NOT_EXIST_REGISTERED_TEAM));
-        return teamMember.getReceivedTeamLikes().stream()
-                .map(LikeResponse::of)
+        return getTeamMemberById(memberProfileId)
+                .getReceivedTeamLikes()
+                .stream()
+                .map(LikeResponse::from)
                 .collect(Collectors.toList());
     }
 
@@ -42,5 +40,10 @@ public class LikeService {
 
     public TeamDetailResponse getTeamDetail(Long likeId) {
         return null;
+    }
+
+    private TeamMember getTeamMemberById(Long memberProfileId) {
+        return teamMemberRepository.getTeamMemberByMemberProfileId(memberProfileId)
+                .orElseThrow(() -> new BadRequestException(ApplicationError.NOT_EXIST_REGISTERED_TEAM));
     }
 }
