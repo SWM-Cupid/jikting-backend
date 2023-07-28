e47da87c421c743adca1fedb89cd8fa184c103a6
jeongyuneo <piq2255@gmail.com>
2023-07-28 15:13:44 +0900

    [SP-178] refactor: ImageResponse 빌더 패턴 삭제 및 정적 팩토리 메소드 적용

--- src/main/java/com/cupid/jikting/member/dto/ImageResponse.java
+++ src/main/java/com/cupid/jikting/member/dto/ImageResponse.java
@@ -1,13 +1,20 @@
 package com.cupid.jikting.member.dto;
 
-import lombok.*;
+import com.cupid.jikting.member.entity.ProfileImage;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageResponse {
 
     private String url;
     private String sequence;
+
+    public static ImageResponse of(ProfileImage profileImage) {
+        return new ImageResponse(profileImage.getUrl(), profileImage.getSequence().name());
+    }
 }
