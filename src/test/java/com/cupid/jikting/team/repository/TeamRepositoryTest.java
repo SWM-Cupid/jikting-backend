59173cb61205ac0c5e059d52722a44bf1727587c
jeongyuneo <piq2255@gmail.com>
2023-07-29 19:32:28 +0900

    [SP-190] test: TeamRepositoryTest 임포트 정리

--- src/test/java/com/cupid/jikting/team/repository/TeamRepositoryTest.java
+++ src/test/java/com/cupid/jikting/team/repository/TeamRepositoryTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
