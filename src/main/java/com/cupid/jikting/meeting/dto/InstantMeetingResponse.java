48aa4eb83cfd7afcdf3e4812ecaa23bbe618c145
smc2315 <choeyongtae085@gmail.com>
2023-07-31 00:56:17 +0900

    [SP-195] refactor: InstantMeetingResponse 정적 팩토리 메서드 적용

--- src/main/java/com/cupid/jikting/meeting/dto/InstantMeetingResponse.java
+++ src/main/java/com/cupid/jikting/meeting/dto/InstantMeetingResponse.java
@@ -1,16 +1,30 @@
 package com.cupid.jikting.meeting.dto;
 
-import lombok.*;
+import com.cupid.jikting.meeting.entity.InstantMeeting;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InstantMeetingResponse {
 
     private Long instantMeetingId;
-    private LocalDateTime instantMeetingDateTime;
-    private String location;
+    private LocalDateTime schedule;
+    private String place;
+    private int memberCount;
+    private boolean isAttend;
+
+    public static InstantMeetingResponse of(InstantMeeting instantMeeting, boolean isAttend) {
+        return new InstantMeetingResponse(
+                instantMeeting.getId(),
+                instantMeeting.getSchedule(),
+                instantMeeting.getPlace(),
+                instantMeeting.getMemberCount(),
+                isAttend);
+    }
 }
