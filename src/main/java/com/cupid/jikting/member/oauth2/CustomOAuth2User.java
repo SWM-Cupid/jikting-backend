dd7d7f3c45848d5bdea560d600d16ae73d814879
smc2315 <choeyongtae085@gmail.com>
2023-07-30 23:35:21 +0900

    [SP-156] style: 개행 수정

--- src/main/java/com/cupid/jikting/member/oauth2/CustomOAuth2User.java
+++ src/main/java/com/cupid/jikting/member/oauth2/CustomOAuth2User.java
@@ -14,8 +14,8 @@ public class CustomOAuth2User extends DefaultOAuth2User {
     private final String username;
     private final Role role;
 
-    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey, String username,
-                            Role role) {
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes,
+                            String nameAttributeKey, String username, Role role) {
         super(authorities, attributes, nameAttributeKey);
         this.username = username;
         this.role = role;
