98910fa08393a675a63f574296dd2fc4d7e79ec9
smc2315 <choeyongtae085@gmail.com>
2023-07-31 14:50:05 +0900

    [SP-195] fix: InstantMeetingRepository에 조회 로직 삭제 및 InstantMeetingRepositoryTest 삭제

--- src/main/java/com/cupid/jikting/meeting/repository/InstantMeetingRepository.java
+++ src/main/java/com/cupid/jikting/meeting/repository/InstantMeetingRepository.java
@@ -3,10 +3,6 @@ package com.cupid.jikting.meeting.repository;
 import com.cupid.jikting.meeting.entity.InstantMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 public interface InstantMeetingRepository extends JpaRepository<InstantMeeting, Long> {
 
-    List<InstantMeeting> findAllByScheduleBetween(LocalDateTime start, LocalDateTime end);
 }
