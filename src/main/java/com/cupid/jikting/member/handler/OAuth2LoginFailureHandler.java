12ba03f174acfcd04236f31c9f0a7ed61bf2f49a
smc2315 <choeyongtae085@gmail.com>
2023-07-31 15:49:33 +0900

    [SP-156] style: 클래스 시작 개행 추가

--- src/main/java/com/cupid/jikting/member/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/cupid/jikting/member/handler/OAuth2LoginFailureHandler.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
