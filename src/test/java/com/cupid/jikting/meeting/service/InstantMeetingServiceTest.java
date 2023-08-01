230f96916cb08ceeb1360e03eb355451db35d2e3
smc2315 <choeyongtae085@gmail.com>
2023-08-01 14:52:23 +0900

    [SP-197] test: 번개팅 참여 테스트 추가

--- src/test/java/com/cupid/jikting/meeting/service/InstantMeetingServiceTest.java
+++ src/test/java/com/cupid/jikting/meeting/service/InstantMeetingServiceTest.java
@@ -1,10 +1,13 @@
 package com.cupid.jikting.meeting.service;
 
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.NotFoundException;
 import com.cupid.jikting.meeting.dto.InstantMeetingResponse;
 import com.cupid.jikting.meeting.entity.InstantMeeting;
 import com.cupid.jikting.meeting.entity.InstantMeetingMember;
 import com.cupid.jikting.meeting.repository.InstantMeetingRepository;
 import com.cupid.jikting.member.entity.MemberProfile;
+import com.cupid.jikting.member.repository.MemberProfileRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,12 +18,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +39,8 @@ public class InstantMeetingServiceTest {
     private static final String PLACE = "장소";
 
     private List<InstantMeeting> instantMeetings;
+    private MemberProfile memberProfile;
+    private InstantMeeting instantMeeting;
 
     @InjectMocks
     InstantMeetingService instantMeetingService;
@@ -38,20 +48,20 @@ public class InstantMeetingServiceTest {
     @Mock
     InstantMeetingRepository instantMeetingRepository;
 
+    @Mock
+    MemberProfileRepository memberProfileRepository;
+
     @BeforeEach
     void setUp() {
-        MemberProfile memberProfile = MemberProfile.builder()
+        memberProfile = MemberProfile.builder()
                 .id(ID)
                 .build();
-        InstantMeetingMember instantMeetingMember = InstantMeetingMember.builder()
-                .memberProfile(memberProfile)
-                .build();
-        InstantMeeting instantMeeting = InstantMeeting.builder()
+        instantMeeting = InstantMeeting.builder()
                 .id(ID)
                 .schedule(SCHEDULE)
                 .place(PLACE)
-                .instantMeetingMembers(List.of(instantMeetingMember))
                 .build();
+        InstantMeetingMember.of(instantMeeting, memberProfile);
         instantMeetings = IntStream.rangeClosed(0, 2)
                 .mapToObj(n -> instantMeeting)
                 .collect(Collectors.toList());
@@ -68,5 +78,41 @@ public class InstantMeetingServiceTest {
                 () -> verify(instantMeetingRepository).findAll(),
                 () -> assertThat(instantMeetingResponses.size()).isEqualTo(instantMeetings.size())
         );
+    }
+
+    @Test
+    void 번개팅_참여_성공() {
+        //given
+        willReturn(Optional.of(memberProfile)).given(memberProfileRepository).findById(anyLong());
+        willReturn(Optional.of(instantMeeting)).given(instantMeetingRepository).findById(anyLong());
+        //when
+        instantMeetingService.attend(ID, ID);
+        //then
+        assertAll(
+                () -> verify(memberProfileRepository).findById(anyLong()),
+                () -> verify(instantMeetingRepository).findById(anyLong()),
+                () -> verify(memberProfileRepository).save(any(MemberProfile.class))
+        );
+    }
+
+    @Test
+    void 번개팅_참여_실패_회원_없음() {
+        // given
+        willThrow(new NotFoundException(ApplicationError.MEMBER_NOT_FOUND)).given(memberProfileRepository).findById(anyLong());
+        // when & then
+        assertThatThrownBy(() -> instantMeetingService.attend(ID, ID))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage(ApplicationError.MEMBER_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 번개팅_참여_실패_번개팅_없음() {
+        // given
+        willReturn(Optional.of(memberProfile)).given(memberProfileRepository).findById(anyLong());
+        willThrow(new NotFoundException(ApplicationError.INSTANT_MEETING_NOT_FOUND)).given(instantMeetingRepository).findById(anyLong());
+        // when & then
+        assertThatThrownBy(() -> instantMeetingService.attend(ID, ID))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage(ApplicationError.INSTANT_MEETING_NOT_FOUND.getMessage());
     }
 }
