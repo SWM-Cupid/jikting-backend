1ec3adb77ece6ebc780e9fc504dafacf47029c69
smc2315 <choeyongtae085@gmail.com>
2023-08-01 11:11:35 +0900

    [SP-156] refactor: 상수화 및 로직 간소화

--- src/main/java/com/cupid/jikting/common/util/PasswordUtil.java
+++ src/main/java/com/cupid/jikting/common/util/PasswordUtil.java
@@ -9,21 +9,20 @@ import java.util.stream.IntStream;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PasswordUtil {
 
-    public static final char[] CHAT_SET = new char[]{
+    private static final char[] CHAT_SET = new char[]{
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     };
-
+    private static final String DELIMITER = "";
+    private static final int PASSWORD_LENGTH = 8;
     private static final Random RANDOM = new Random();
 
     public static String generateRandomPassword() {
-        StringBuilder password = new StringBuilder();
-        IntStream.range(0, 8)
+        return String.join(DELIMITER, IntStream.range(0, PASSWORD_LENGTH)
                 .mapToObj(i -> CHAT_SET[(int) (CHAT_SET.length * RANDOM.nextDouble())])
-                .forEach(password::append);
-        return password.toString();
+                .toArray(CharSequence[]::new));
     }
 }
