19a432f5c8bb3cc6f04454ccfceedc17dc401f33
jeongyuneo <piq2255@gmail.com>
2023-08-03 14:09:01 +0900

    [SP-185] feat: Meeting 미팅 확정 메소드 추가

--- src/main/java/com/cupid/jikting/meeting/entity/Meeting.java
+++ src/main/java/com/cupid/jikting/meeting/entity/Meeting.java
@@ -1,6 +1,8 @@
 package com.cupid.jikting.meeting.entity;
 
 import com.cupid.jikting.common.entity.BaseEntity;
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.WrongAccessException;
 import com.cupid.jikting.team.entity.Team;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -41,5 +43,18 @@ public class Meeting extends BaseEntity {
             return acceptingTeam.getName();
         }
         return recommendedTeam.getName();
+    }
+
+    public void confirm(Long meetingId, LocalDateTime schedule, String place) {
+        validate(meetingId);
+        this.schedule = schedule;
+        this.place = place;
+        confirmStatus = ConfirmStatus.DECIDED;
+    }
+
+    private void validate(Long meetingId) {
+        if (!id.equals(meetingId)) {
+            throw new WrongAccessException(ApplicationError.WRONG_ACCESS);
+        }
     }
 }
