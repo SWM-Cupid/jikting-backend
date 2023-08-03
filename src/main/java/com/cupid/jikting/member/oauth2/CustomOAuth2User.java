0012aeece8d22b68ad97544e9efb7b3f3654f871
jeongyuneo <piq2255@gmail.com>
2023-08-03 21:24:53 +0900

    [SP-175] style: CustomOAuth2UserService 코드 포매팅

--- src/main/java/com/cupid/jikting/member/oauth2/CustomOAuth2User.java
+++ src/main/java/com/cupid/jikting/member/oauth2/CustomOAuth2User.java
@@ -15,7 +15,7 @@ public class CustomOAuth2User extends DefaultOAuth2User {
     private final Role role;
 
     private CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes,
-                            String nameAttributeKey, String username, Role role) {
+                             String nameAttributeKey, String username, Role role) {
         super(authorities, attributes, nameAttributeKey);
         this.username = username;
         this.role = role;
