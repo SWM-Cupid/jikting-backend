6cac3c10a68b89b645333a87a3f735388a350b9f
jeongyuneo <piq2255@gmail.com>
2023-08-03 21:23:55 +0900

    [SP-175] style: RedisConfig 미사용 임포트 삭제

--- src/main/java/com/cupid/jikting/common/config/RedisConfig.java
+++ src/main/java/com/cupid/jikting/common/config/RedisConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
