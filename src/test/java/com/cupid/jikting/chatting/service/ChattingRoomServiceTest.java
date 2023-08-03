92252056aea5473773e656b0b6a299fb69b82cf2
jeongyuneo <piq2255@gmail.com>
2023-08-03 14:10:40 +0900

    [SP-185] test: ChattingRoomServiceTest 미팅 확정 성공 테스트 추가

--- src/test/java/com/cupid/jikting/chatting/service/ChattingRoomServiceTest.java
+++ src/test/java/com/cupid/jikting/chatting/service/ChattingRoomServiceTest.java
@@ -1,6 +1,7 @@
 package com.cupid.jikting.chatting.service;
 
 import com.cupid.jikting.chatting.dto.ChattingRoomResponse;
+import com.cupid.jikting.chatting.dto.MeetingConfirmRequest;
 import com.cupid.jikting.chatting.entity.ChattingRoom;
 import com.cupid.jikting.chatting.repository.ChattingRoomRepository;
 import com.cupid.jikting.common.error.ApplicationError;
@@ -18,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,9 +27,12 @@ import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.verify;
 
 @WebMvcTest(MockitoExtension.class)
 class ChattingRoomServiceTest {
@@ -35,8 +40,11 @@ class ChattingRoomServiceTest {
     private static final Long ID = 1L;
     private static final String TEAM_NAME = "팀 이름";
     private static final boolean LEADER = true;
+    private static final LocalDateTime SCHEDULE = LocalDateTime.of(2023, 9, 10, 18, 30);
+    private static final String PLACE = "장소";
 
     private MemberProfile memberProfile;
+    private ChattingRoom chattingRoom;
     private List<ChattingRoom> chattingRooms;
     private ApplicationException memberNotFoundException;
 
@@ -59,15 +67,16 @@ class ChattingRoomServiceTest {
                 .id(ID)
                 .build();
         TeamMember.of(LEADER, team, memberProfile);
-        chattingRooms = IntStream.range(0, 3)
-                .mapToObj(n -> ChattingRoom.builder()
-                        .id(n + ID)
-                        .meeting(Meeting.builder()
-                                .id(ID)
-                                .recommendedTeam(team)
-                                .acceptingTeam(team)
-                                .build())
+        chattingRoom = ChattingRoom.builder()
+                .id(ID)
+                .meeting(Meeting.builder()
+                        .id(ID)
+                        .recommendedTeam(team)
+                        .acceptingTeam(team)
                         .build())
+                .build();
+        chattingRooms = IntStream.range(0, 3)
+                .mapToObj(n -> chattingRoom)
                 .collect(Collectors.toList());
         memberNotFoundException = new NotFoundException(ApplicationError.MEMBER_NOT_FOUND);
     }
@@ -91,5 +100,24 @@ class ChattingRoomServiceTest {
         assertThatThrownBy(() -> chattingRoomService.getAll(ID))
                 .isInstanceOf(NotFoundException.class)
                 .hasMessage(ApplicationError.MEMBER_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 채팅방_내_미팅_화정_성공() {
+        // given
+        willReturn(Optional.of(chattingRoom)).given(chattingRoomRepository).findById(anyLong());
+        willReturn(chattingRoom).given(chattingRoomRepository).save(any(ChattingRoom.class));
+        MeetingConfirmRequest meetingConfirmRequest = MeetingConfirmRequest.builder()
+                .meetingId(ID)
+                .schedule(SCHEDULE)
+                .place(PLACE)
+                .build();
+        // when
+        chattingRoomService.confirm(ID, meetingConfirmRequest);
+        // then
+        assertAll(
+                () -> verify(chattingRoomRepository).findById(anyLong()),
+                () -> verify(chattingRoomRepository).save(any(ChattingRoom.class))
+        );
     }
 }
