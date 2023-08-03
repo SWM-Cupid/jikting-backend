9bd414a94c54e8a83b9b05c5754c38610a1196fd
jeongyuneo <piq2255@gmail.com>
2023-08-03 19:36:50 +0900

    [SP-175] feat: VerificationCodeGenerator 추가

---
+++ src/main/java/com/cupid/jikting/common/util/VerificationCodeGenerator.java
@@ -0,0 +1,22 @@
+package com.cupid.jikting.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VerificationCodeGenerator {
+
+    private static final Random RANDOM = new Random();
+    private static final String DELIMITER = "";
+    private static final int VERIFICATION_CODE_RANGE = 9;
+
+    public static String generate(int verificationCodeLength) {
+        return String.join(DELIMITER, IntStream.range(0, verificationCodeLength)
+                .mapToObj(n -> RANDOM.nextInt(VERIFICATION_CODE_RANGE))
+                .map(String::valueOf)
+                .toArray(CharSequence[]::new));
+    }
+}
