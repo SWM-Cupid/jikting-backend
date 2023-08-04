80ca940658284357c4eb9ce59c6417f4e575779c
jeongyuneo <piq2255@gmail.com>
2023-08-04 20:48:46 +0900

    [SP-176] feat: PhoneVerificationRequest 추가

---
+++ src/main/java/com/cupid/jikting/member/dto/PhoneVerificationRequest.java
@@ -0,0 +1,13 @@
+package com.cupid.jikting.member.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PhoneVerificationRequest {
+
+    private String phone;
+    private String verificationCode;
+}
