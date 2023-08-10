a2f43b613d4782157a07ba5baf56ad3c40d00e87
jeongyuneo <piq2255@gmail.com>
2023-08-10 14:02:55 +0900

    [SP-240] feat: RedisSubscriber 추가

---
+++ src/main/java/com/cupid/jikting/chatting/service/RedisChattingService.java
@@ -0,0 +1,30 @@
+package com.cupid.jikting.chatting.service;
+
+import com.cupid.jikting.chatting.dto.ChattingRequest;
+import com.cupid.jikting.common.service.RedisConnector;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Primary
+@Transactional
+@Service
+public class ㄷ implements ChattingService {
+
+    private static final String CHATTING_ROOM = "CHATTING_ROOM";
+    private static final String DELIMITER = ":";
+
+    private final RedisPublisher redisPublisher;
+    private final RedisMessageListener redisMessageListener;
+    private final RedisConnector redisConnector;
+
+    @Override
+    public void sendMessage(Long chattingRoomId, ChattingRequest chattingRequest) {
+        redisPublisher.publish(redisMessageListener.getTopic(chattingRoomId), chattingRequest);
+        redisConnector.saveChatting(CHATTING_ROOM + DELIMITER + chattingRoomId, chattingRequest.toChatting());
+    }
+}
