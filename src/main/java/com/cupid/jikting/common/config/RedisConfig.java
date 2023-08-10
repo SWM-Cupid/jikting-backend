5b01b155776ba4a3a8b3386fa8491568d4938375
jeongyuneo <piq2255@gmail.com>
2023-08-10 13:27:09 +0900

    [SP-240] feat: RedisConfig MessageListenerContainer 빈 등록

--- src/main/java/com/cupid/jikting/common/config/RedisConfig.java
+++ src/main/java/com/cupid/jikting/common/config/RedisConfig.java
@@ -1,17 +1,25 @@
 package com.cupid.jikting.common.config;
 
+import com.cupid.jikting.chatting.service.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.List;
 
 @Configuration
 public class RedisConfig {
+
+    private static final String TOPIC_NAME = "topic";
 
     @Value("${spring.redis.cluster.nodes}")
     private List<String> clusterNodes;
@@ -28,5 +36,25 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(new RedisClusterConfiguration(clusterNodes));
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
+                                                                       MessageListenerAdapter listenerAdapter,
+                                                                       ChannelTopic channelTopic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, channelTopic);
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "onMessage");
+    }
+
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic(TOPIC_NAME);
     }
 }
