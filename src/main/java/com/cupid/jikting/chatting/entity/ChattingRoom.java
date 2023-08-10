28f4a155cb9cf714f2db56c7af00bb09d7a2721e
jeongyuneo <piq2255@gmail.com>
2023-08-10 13:57:59 +0900

    [SP-240] feat: ChattingRoom 채팅 연관관계 필드 및 메소드 삭제

--- src/main/java/com/cupid/jikting/chatting/entity/ChattingRoom.java
+++ src/main/java/com/cupid/jikting/chatting/entity/ChattingRoom.java
@@ -2,14 +2,12 @@ package com.cupid.jikting.chatting.entity;
 
 import com.cupid.jikting.common.entity.BaseEntity;
 import com.cupid.jikting.meeting.entity.Meeting;
-import com.cupid.jikting.member.entity.MemberProfile;
 import com.cupid.jikting.team.entity.Team;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,27 +19,15 @@ import java.util.List;
 @SQLDelete(sql = "UPDATE chatting SET deleted = true WHERE id=?")
 @AttributeOverride(name = "id", column = @Column(name = "chatting_room_id"))
 @Entity
-public class ChattingRoom extends BaseEntity implements Serializable {
+public class ChattingRoom extends BaseEntity {
 
     @OneToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "meeting_id")
     private Meeting meeting;
 
     @Builder.Default
-    @OneToMany(mappedBy = "chattingRoom", cascade = CascadeType.ALL)
-    private List<Chatting> chattings = new ArrayList<>();
-
-    @Builder.Default
     @OneToMany(mappedBy = "chattingRoom", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<MemberChattingRoom> memberChattingRooms = new ArrayList<>();
-
-    public void createChatting(Chatting chatting) {
-        chattings.add(chatting);
-        MemberProfile sender = chatting.getMemberProfile();
-        MemberChattingRoom memberChattingRoom = MemberChattingRoom.of(sender, this);
-        memberChattingRooms.add(memberChattingRoom);
-        sender.addMemberChattingRoom(memberChattingRoom);
-    }
 
     public String getOppositeTeamName(Team team) {
         return meeting.getOppositeTeamName(team);
