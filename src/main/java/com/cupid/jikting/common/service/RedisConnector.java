75b0bbd976982f662ed6243a26572860c8eff6b1
jeongyuneo <piq2255@gmail.com>
2023-08-10 14:04:58 +0900

    [SP-240] feat: RedisConnector RedisTemplate 타입 수정 및 채팅 관련 메소드 추가

--- src/main/java/com/cupid/jikting/common/service/RedisConnector.java
+++ src/main/java/com/cupid/jikting/common/service/RedisConnector.java
@@ -1,8 +1,13 @@
 package com.cupid.jikting.common.service;
 
+import com.cupid.jikting.chatting.dto.ChattingRequest;
+import com.cupid.jikting.chatting.entity.Chatting;
 import com.cupid.jikting.common.error.ApplicationError;
 import com.cupid.jikting.common.error.BadRequestException;
+import com.cupid.jikting.common.util.JsonParser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
@@ -10,19 +15,41 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class RedisConnector {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private static final String NO_MESSAGE = "";
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final JsonParser jsonParser;
 
     public void set(String key, String value, int expireTime) {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
         valueOperations.set(key, value, Duration.ofMinutes(expireTime));
     }
 
     public String get(String key) {
         return Optional.ofNullable(redisTemplate.opsForValue().get(key))
+                .map(String::valueOf)
                 .orElseThrow(() -> new BadRequestException(ApplicationError.VERIFICATION_CODE_EXPIRED));
+    }
+
+    public void saveChatting(String chattingRoom, Chatting chatting) {
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        hashOperations.put(chattingRoom, chatting.getId(), chatting);
+        log.info("create new chatting [{}] int chatting room '{}'", chatting, chattingRoom);
+    }
+
+    public String getLastMessage(String chattingRoom) {
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        return hashOperations.values(chattingRoom)
+                .stream()
+                .map(String::valueOf)
+                .map(jsonParser::toChattingRequest)
+                .findFirst()
+                .map(ChattingRequest::getContent)
+                .orElse(NO_MESSAGE);
     }
 }
