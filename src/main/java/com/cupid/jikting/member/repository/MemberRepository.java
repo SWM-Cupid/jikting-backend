9d439f3cbc27fc0c217dede7d24bbb4ed782ad96
smc2315 <choeyongtae085@gmail.com>
2023-08-11 19:19:45 +0900

    [SP-305] fix: MemberRepository findByRefreshToken 삭제

--- src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
+++ src/main/java/com/cupid/jikting/member/repository/MemberRepository.java
@@ -10,8 +10,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
 
-    Optional<Member> findByRefreshToken(String refreshToken);
-
     boolean existsByUsername(String username);
 
     Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
