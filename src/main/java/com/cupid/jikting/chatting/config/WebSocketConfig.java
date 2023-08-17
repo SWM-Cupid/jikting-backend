c74384686164bd1f6a1fa6bdf68ffaf2f01c4351
jeongyuneo <piq2255@gmail.com>
2023-08-17 16:44:24 +0900

    [SP-311] feat: WebSocketConfig 엔드포인트 수정

--- src/main/java/com/cupid/jikting/chatting/config/WebSocketConfig.java
+++ src/main/java/com/cupid/jikting/chatting/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chattings")
+        registry.addEndpoint("/ws/chattings")
                 .setAllowedOrigins("*");
     }
 
