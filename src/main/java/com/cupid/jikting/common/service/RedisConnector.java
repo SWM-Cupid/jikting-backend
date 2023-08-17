589f16b656f4612825594d2ef9ea960774831a59
jeongyuneo <piq2255@gmail.com>
2023-08-17 14:21:43 +0900

    [SP-311] feat: RedisConnector 채팅 내역 조회 메소드 추가

--- src/main/java/com/cupid/jikting/common/service/RedisConnector.java
+++ src/main/java/com/cupid/jikting/common/service/RedisConnector.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -62,5 +63,10 @@ public class RedisConnector {
 
     public void delete(String key) {
         redisTemplate.delete(key);
+    }
+
+    public List<Chatting> getMessages(String chattingRoomKey) {
+        HashOperations<String, Long, Chatting> hashOperations = redisTemplate.opsForHash();
+        return hashOperations.values(chattingRoomKey);
     }
 }
