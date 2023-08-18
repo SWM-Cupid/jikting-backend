c4b4853f14d849e90711f9a8d2040ae6b0ccd85a
jeongyuneo <piq2255@gmail.com>
2023-08-18 10:22:18 +0900

    [SP-332] fix: ChattingRoomService 채팅방 목록 조회 로직 수정

--- src/main/java/com/cupid/jikting/chatting/service/ChattingRoomService.java
+++ src/main/java/com/cupid/jikting/chatting/service/ChattingRoomService.java
@@ -3,13 +3,11 @@ package com.cupid.jikting.chatting.service;
 import com.cupid.jikting.chatting.dto.*;
 import com.cupid.jikting.chatting.entity.Chatting;
 import com.cupid.jikting.chatting.entity.ChattingRoom;
-import com.cupid.jikting.chatting.entity.MemberChattingRoom;
 import com.cupid.jikting.chatting.repository.ChattingRoomRepository;
 import com.cupid.jikting.common.error.ApplicationError;
 import com.cupid.jikting.common.error.NotFoundException;
 import com.cupid.jikting.common.service.RedisConnector;
 import com.cupid.jikting.member.entity.MemberProfile;
-import com.cupid.jikting.member.entity.ProfileImage;
 import com.cupid.jikting.member.repository.MemberProfileRepository;
 import com.cupid.jikting.team.entity.Team;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,9 +37,9 @@ public class ChattingRoomService {
 
     public List<ChattingRoomResponse> getAll(Long memberProfileId) {
         MemberProfile memberProfile = getMemberProfileById(memberProfileId);
-        return chattingRoomRepository.findAll()
+        return memberProfile.getChattingRooms()
                 .stream()
-                .map(chattingRoom -> toChattingRoomResponse(chattingRoom, memberProfile.getTeam()))
+                .map(chattingRoom -> getChattingRoomResponse(chattingRoom, memberProfile.getTeam()))
                 .collect(Collectors.toList());
     }
 
@@ -70,23 +67,13 @@ public class ChattingRoomService {
                 .orElseThrow(() -> new NotFoundException(ApplicationError.MEMBER_NOT_FOUND));
     }
 
-    private ChattingRoomResponse toChattingRoomResponse(ChattingRoom chattingRoom, Team team) {
+    private ChattingRoomResponse getChattingRoomResponse(ChattingRoom chattingRoom, Team team) {
         return ChattingRoomResponse.builder()
                 .chattingRoomId(chattingRoom.getId())
                 .opposingTeamName(chattingRoom.getOppositeTeamName(team))
                 .lastMessage(redisConnector.getLastMessage(CHATTING_ROOM_KEY_HEADER + chattingRoom.getId()))
-                .images(getImages(chattingRoom.getMemberChattingRooms()))
+                .images(chattingRoom.getOppositeTeamImageUrls(team))
                 .build();
-    }
-
-    private List<String> getImages(List<MemberChattingRoom> memberChattingRooms) {
-        return memberChattingRooms.stream()
-                .flatMap(memberChattingRoom -> Arrays.stream(memberChattingRoom.getMemberProfile()
-                        .getProfileImages()
-                        .stream()
-                        .map(ProfileImage::getUrl)
-                        .toArray(String[]::new)))
-                .collect(Collectors.toList());
     }
 
     private ChattingRoom getChattingRoomById(Long chattingRoomId) {
