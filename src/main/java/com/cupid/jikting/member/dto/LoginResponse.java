b398f6c895b43d1c1615933c00363996f3b39f51
smc2315 <choeyongtae085@gmail.com>
2023-09-12 15:11:57 +0900

    [SP-377] feat: LoginResponse 추가

---
+++ src/main/java/com/cupid/jikting/member/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.cupid.jikting.member.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginResponse {
+
+    private Long memberProfileId;
+    private String role;
+}
