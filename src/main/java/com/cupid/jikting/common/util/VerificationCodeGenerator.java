c3d29121c27747d998abba6f4b1e27c83ff6e607
jeongyuneo <piq2255@gmail.com>
2023-09-12 16:19:01 +0900

    [SP-243] refactor: VerificationCodeGenerator 랜덤 생성 공통 로직 RandomGenerator로 추출

--- src/main/java/com/cupid/jikting/common/util/VerificationCodeGenerator.java
+++ src/main/java/com/cupid/jikting/common/util/VerificationCodeGenerator.java
@@ -3,28 +3,17 @@ package com.cupid.jikting.common.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.stream.IntStream;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class VerificationCodeGenerator {
+public class VerificationCodeGenerator extends RandomGenerator {
 
-    private static final String DELIMITER = "";
     private static final int VERIFICATION_CODE_RANGE = 10;
 
     public static String generate(int verificationCodeLength) {
         return String.join(DELIMITER, IntStream.range(0, verificationCodeLength)
-                .mapToObj(n -> getSecureRandom().nextInt(VERIFICATION_CODE_RANGE))
+                .mapToObj(n -> RANDOM.nextInt(VERIFICATION_CODE_RANGE))
                 .map(String::valueOf)
                 .toArray(CharSequence[]::new));
-    }
-
-    private static SecureRandom getSecureRandom() {
-        try {
-            return SecureRandom.getInstanceStrong();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
