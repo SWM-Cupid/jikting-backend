90eaeb71459688937d6a5894d2fb67db07a16484
jeongyuneo <piq2255@gmail.com>
2023-09-13 15:55:22 +0900

    [SP-243] style: TeamNameGenerator 배열 상수 초기화 코드 수정

--- src/main/java/com/cupid/jikting/common/util/TeamNameGenerator.java
+++ src/main/java/com/cupid/jikting/common/util/TeamNameGenerator.java
@@ -6,10 +6,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TeamNameGenerator extends RandomGenerator {
 
-    private static final String[] COLORS = new String[]{
+    private static final String[] COLORS = {
             "하양", "검정", "빨강", "주황", "노랑", "초록", "파랑", "보라", "분홍"
     };
-    private static final String[] ANIMALS = new String[]{
+    private static final String[] ANIMALS = {
             "강아지", "고양이", "오리", "사자", "호랑이", "판다", "코끼리", "기린", "원숭이", "캥거루", "악어", "상어", "사슴", "토끼",
             "거북이", "여우", "쿼카", "앵무새", "다람쥐", "너구리", "알파카", "치타", "펭귄", "햄스터", "미어캣", "수달", "표범"
     };
