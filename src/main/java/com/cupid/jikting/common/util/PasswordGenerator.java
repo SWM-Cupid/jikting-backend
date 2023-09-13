6f8749ce8801ee5257bef6fbc1b041d45726e06e
jeongyuneo <piq2255@gmail.com>
2023-09-13 15:56:25 +0900

    [SP-243] refactor: PasswordGenerator 문자 배열 타입 수정

--- src/main/java/com/cupid/jikting/common/util/PasswordGenerator.java
+++ src/main/java/com/cupid/jikting/common/util/PasswordGenerator.java
@@ -8,18 +8,18 @@ import java.util.stream.IntStream;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PasswordGenerator extends RandomGenerator {
 
-    private static final char[] CHAT_SET = new char[]{
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    private static final String[] CHARACTERS = {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     };
     private static final int PASSWORD_LENGTH = 8;
 
     public static String generate() {
         return String.join(DELIMITER, IntStream.range(0, PASSWORD_LENGTH)
-                .mapToObj(i -> CHAT_SET[(int) (CHAT_SET.length * RANDOM.nextDouble())])
-                .toArray(CharSequence[]::new));
+                .mapToObj(i -> CHARACTERS[(int) (CHARACTERS.length * RANDOM.nextDouble())])
+                .toArray(String[]::new));
     }
 }
