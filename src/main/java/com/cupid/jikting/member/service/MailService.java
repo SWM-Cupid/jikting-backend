cb6a40900463618acae633740f5c0d89f34aa238
jeongyuneo <piq2255@gmail.com>
2023-09-25 21:02:26 +0900

    [SP-413] feat: MailService 추가

---
+++ src/main/java/com/cupid/jikting/member/service/MailService.java
@@ -0,0 +1,12 @@
+package com.cupid.jikting.member.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public interface MailService {
+
+    void sendMail(String name, String email) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException;
+}
