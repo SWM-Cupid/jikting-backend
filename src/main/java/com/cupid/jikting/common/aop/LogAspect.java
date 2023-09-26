f70aa5db66e88d8b2b6f9bf394b38d2111935944
smc2315 <choeyongtae085@gmail.com>
2023-09-26 20:11:06 +0900

    [SP-384] feat: Log AOP 추가

---
+++ src/main/java/com/cupid/jikting/common/aop/LogAspect.java
@@ -0,0 +1,20 @@
+package com.cupid.jikting.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Slf4j
+@Component
+@Aspect
+public class LogAspect {
+
+    @Before(value = "within(com.cupid.jikting..controller.*)")
+    public void beforeRequest(JoinPoint joinPoint) {
+        log.info("메소드 선언부 : {}, 전달 파라미터 : {}", joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
+    }
+}
