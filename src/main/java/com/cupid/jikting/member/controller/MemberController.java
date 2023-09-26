ba8c44285cbdefafbe6fe470b16038ce1d4b04cd
jeongyuneo <piq2255@gmail.com>
2023-09-26 20:34:56 +0900

    [SP-413] refactor: MemberController sms 전송 메소드 예외 추상화

--- src/main/java/com/cupid/jikting/member/controller/MemberController.java
+++ src/main/java/com/cupid/jikting/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.cupid.jikting.member.controller;
 import com.cupid.jikting.common.support.AuthorizedVariable;
 import com.cupid.jikting.member.dto.*;
 import com.cupid.jikting.member.service.MemberService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @RequiredArgsConstructor
 @RestController
@@ -78,7 +74,7 @@ public class MemberController {
 
     @PostMapping("/code")
     public ResponseEntity<Void> createVerificationCodeForSignup(@RequestBody SignUpVerificationCodeRequest signUpVerificationCodeRequest)
-            throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+            throws Exception {
         memberService.createVerificationCodeForSignup(signUpVerificationCodeRequest);
         return ResponseEntity.ok().build();
     }
@@ -91,7 +87,7 @@ public class MemberController {
 
     @PostMapping("/username/search/code")
     public ResponseEntity<Void> createVerificationCodeForSearchUsername(@RequestBody UsernameSearchVerificationCodeRequest usernameSearchVerificationCodeRequest)
-            throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+            throws Exception {
         memberService.createVerificationCodeForSearchUsername(usernameSearchVerificationCodeRequest);
         return ResponseEntity.ok().build();
     }
@@ -103,7 +99,7 @@ public class MemberController {
 
     @PostMapping("/password/reset/code")
     public ResponseEntity<Void> createVerificationCodeForResetPassword(@RequestBody PasswordResetVerificationCodeRequest passwordResetVerificationCodeRequest)
-            throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+            throws Exception {
         memberService.createVerificationCodeForResetPassword(passwordResetVerificationCodeRequest);
         return ResponseEntity.ok().build();
     }
