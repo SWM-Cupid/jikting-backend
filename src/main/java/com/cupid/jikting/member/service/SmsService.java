b1262e5ccf717e2536ca05198ba81bca7ecf4d25
jeongyuneo <piq2255@gmail.com>
2023-09-26 20:34:27 +0900

    [SP-413] refactor: SmsService sms 전송 메소드 예외 추상화

--- src/main/java/com/cupid/jikting/member/service/SmsService.java
+++ src/main/java/com/cupid/jikting/member/service/SmsService.java
@@ -1,13 +1,8 @@
 package com.cupid.jikting.member.service;
 
 import com.cupid.jikting.member.dto.SendSmsRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public interface SmsService {
 
-    void sendSms(SendSmsRequest sendSmsRequest) throws JsonProcessingException, NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException;
+    void sendSms(SendSmsRequest sendSmsRequest) throws Exception;
 }
