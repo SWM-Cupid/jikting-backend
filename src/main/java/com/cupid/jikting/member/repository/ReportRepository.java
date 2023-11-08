44140616bbff25c1fc589696d3628356d1298c65
smc2315 <choeyongtae085@gmail.com>
2023-11-08 16:41:34 +0900

    [SP-380] feat: ReportRepository 추가

---
+++ src/main/java/com/cupid/jikting/member/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package com.cupid.jikting.member.repository;
+
+import com.cupid.jikting.member.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
