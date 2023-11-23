5c125666499abdf05ff0d27237240f58ff04e8f6
jeongyuneo <piq2255@gmail.com>
2023-11-24 01:32:13 +0900

    [SP-441] feat: TeamRepository CustomTeamRepository 구현 추가

--- src/main/java/com/cupid/jikting/team/repository/TeamRepository.java
+++ src/main/java/com/cupid/jikting/team/repository/TeamRepository.java
@@ -3,7 +3,7 @@ package com.cupid.jikting.team.repository;
 import com.cupid.jikting.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, Long>, CustomTeamRepository {
 
     boolean existsByName(String name);
 }
