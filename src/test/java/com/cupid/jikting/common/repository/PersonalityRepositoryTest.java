669058135f320e5dcc393a91521859a9752ac094
jeongyuneo <62989828+jeongyuneo@users.noreply.github.com>
2023-12-06 18:57:45 +0900

    Revert "[SP-443] 레포지토리 테스트 삭제"

---
+++ src/test/java/com/cupid/jikting/common/repository/PersonalityRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.cupid.jikting.common.repository;
+
+import com.cupid.jikting.common.entity.Personality;
+import com.cupid.jikting.common.error.ApplicationError;
+import com.cupid.jikting.common.error.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PersonalityRepositoryTest {
+
+    private static final String KEYWORD = "성격 키워드";
+
+    @Autowired
+    private PersonalityRepository personalityRepository;
+
+    @BeforeEach
+    void setUp() {
+        personalityRepository.save(
+                Personality.builder()
+                        .keyword(KEYWORD)
+                        .build());
+    }
+
+    @Test
+    void 키워드로_성격_조회_성공() {
+        // when
+        Personality personality = personalityRepository.findByKeyword(KEYWORD)
+                .orElseThrow(() -> new NotFoundException(ApplicationError.PERSONALITY_NOT_FOUND));
+        // then
+        assertThat(personality.getKeyword()).isEqualTo(KEYWORD);
+    }
+}
---
+++ src/test/java/com/cupid/jikting/team/repository/TeamRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.cupid.jikting.team.repository;
+
+import com.cupid.jikting.common.repository.PersonalityRepository;
+import com.cupid.jikting.member.entity.Gender;
+import com.cupid.jikting.team.entity.Team;
+import com.cupid.jikting.team.entity.TeamPersonality;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TeamRepositoryTest {
+
+    private static final String NAME = "이름";
+    private static final int MEMBER_COUNT = 3;
+    private static final String DESCRIPTION = "한줄소개";
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private PersonalityRepository personalityRepository;
+
+    @Test
+    void 팀_저장_성공() {
+        // given
+        Team team = Team.builder()
+                .name(NAME)
+                .memberCount(MEMBER_COUNT)
+                .description(DESCRIPTION)
+                .gender(Gender.MALE)
+                .build();
+        List<TeamPersonality> teamPersonalities = personalityRepository.findAll()
+                .stream()
+                .map(personality -> TeamPersonality.builder()
+                        .team(team)
+                        .personality(personality)
+                        .build())
+                .collect(Collectors.toList());
+        team.addTeamPersonalities(teamPersonalities);
+        // when
+        Team savedTeam = teamRepository.save(team);
+        // then
+        assertAll(
+                () -> assertThat(savedTeam.getName()).isEqualTo(NAME),
+                () -> assertThat(savedTeam.getMemberCount()).isEqualTo(MEMBER_COUNT),
+                () -> assertThat(savedTeam.getDescription()).isEqualTo(DESCRIPTION),
+                () -> assertThat(savedTeam.getGender()).isEqualTo(Gender.MALE),
+                () -> assertThat(savedTeam.getTeamPersonalities()).hasSize(teamPersonalities.size())
+        );
+    }
+
+    @Test
+    void 팀_삭제_성공() {
+        // given
+        Team team = Team.builder()
+                .name(NAME)
+                .memberCount(MEMBER_COUNT)
+                .description(DESCRIPTION)
+                .build();
+        Team savedTeam = teamRepository.save(team);
+        // when
+        teamRepository.delete(team);
+        // then
+        assertThat(teamRepository.findById(savedTeam.getId())).isEmpty();
+    }
+}
